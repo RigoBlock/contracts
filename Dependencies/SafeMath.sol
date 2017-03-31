7b00d0c06d29364691b21ca809229d751b6f9c30
escursionista <gabriele@rigoinvestment.com>
2017-03-31 18:18:12 +0200

    Create SafeMath.sol

---
+++ Dependencies/SafeMath.sol
@@ -0,0 +1,50 @@
+//! SafeMath contract.
+//! By Gabriele Rigo (Rigo Investment), 2017.
+//! Released under the Apache Licence 2.
+//! Avoids overflows!
+
+pragma solidity ^0.4.10;
+
+contract SafeMath {
+
+    function safeMul(uint a, uint b) internal returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+    
+    function safeDiv(uint a, uint b) internal returns (uint) {
+        assert(b > 0);
+        uint c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
+
+    function safeSub(uint a, uint b) internal returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function safeAdd(uint a, uint b) internal returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
+    }
+    
+    function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+        return a >= b ? a : b;
+    }
+
+    function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+        return a < b ? a : b;
+    }
+
+    function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+        return a < b ? a : b;
+    }
+
+}
