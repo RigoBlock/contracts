7f8cf8270bb094f40ad2e8d30fa52cf6d2bdc45c
escursionista <gabriele@rigoinvestment.com>
2017-03-31 17:55:19 +0200

    Update ERC20.sol

--- Dependencies/ERC20.sol
+++ Dependencies/ERC20.sol
@@ -1,9 +1,15 @@
-contract ERC20Face {
+//! ERC20 Token Interface.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
 
-  event Transfer(address indexed _from, address indexed _to, uint256 _value) {}
+pragma solidity ^0.4.10;
+
+contract ERC20Face {
+	
+	event Transfer(address indexed _from, address indexed _to, uint256 _value) {}
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value) {}
   
-  function transfer(address _to, uint256 _value) returns (bool success) {}
+  	function transfer(address _to, uint256 _value) returns (bool success) {}
 	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 	function approve(address _spender, uint256 _value) returns (bool success) {}
 
