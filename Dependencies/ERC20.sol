803d0fb124408dad6965054c9cc8d91fb614464b
escursionista <gabriele@rigoinvestment.com>
2017-03-31 17:57:43 +0200

    Update ERC20.sol

--- Dependencies/ERC20.sol
+++ Dependencies/ERC20.sol
@@ -5,10 +5,14 @@
 pragma solidity ^0.4.10;
 
 contract ERC20Face {
+
+	//EVENTS
 	
 	event Transfer(address indexed _from, address indexed _to, uint256 _value) {}
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value) {}
   
+  	//METHODS
+	
   	function transfer(address _to, uint256 _value) returns (bool success) {}
 	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 	function approve(address _spender, uint256 _value) returns (bool success) {}
