0e8924d011b701551d45611c03efd4343839e64b
escursionista <gabriele@rigoinvestment.com>
2017-04-01 11:26:43 +0200

    Update DragoRegistry.sol

--- Registries/DragoRegistry.sol
+++ Registries/DragoRegistry.sol
@@ -4,11 +4,11 @@
 
 pragma solidity ^0.4.10;
 
-contract DragooRegistry {
+contract DragoRegistry is DragoRegistryFace {
     
-    //separate registry, present here only abstract registry
+	//separate registry, present here only abstract registry
     
-    //modifier when_address_free(address _addr) { if (mapFromAddress[_addr] != 0) return; _; }
+	//modifier when_address_free(address _addr) { if (mapFromAddress[_addr] != 0) return; _; }
 	//modifier when_name_free(bytes32 _name) { if (mapFromName[_name] != 0) return; _; }
 	//modifier when_has_name(bytes32 _name) { if (mapFromName[_name] == 0) return; _; }
 	//modifier only_badge_owner(uint _id) { if (badges[_id].owner != msg.sender) return; _; }
