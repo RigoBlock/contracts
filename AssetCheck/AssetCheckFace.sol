1c32acc0f0ae447b81fe636d56588251b66edb38
escursionista <gabriele@rigoinvestment.com>
2017-04-01 12:20:25 +0200

    Create AssetCheckFace.sol

---
+++ AssetCheck/AssetCheckFace.sol
@@ -0,0 +1,9 @@
+//! Asset approved contract.
+//! By Gabriele Rigo (Rigo Investment), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
+contract AssetCheckFace {
+      function assetCheck(address _asset) public constant returns (bool _approved) {}
+}
