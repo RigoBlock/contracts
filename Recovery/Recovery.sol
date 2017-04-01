e8ffaea81f288b6f2244230f7ebf5762f58bde4d
escursionista <gabriele@rigoinvestment.com>
2017-04-01 11:42:39 +0200

    Create Recovery.sol

---
+++ Recovery/Recovery.sol
@@ -0,0 +1,7 @@
+//! Recovery contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
+contract Recovery is RecoveryFace {}
