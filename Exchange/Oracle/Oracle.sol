56b76c5f47aa3380ad0418001fb214ba3f4f330c
escursionista <gabriele@rigoinvestment.com>
2017-04-01 18:22:50 +0200

    Create Oracle.sol

---
+++ Exchange/Oracle/Oracle.sol
@@ -0,0 +1,33 @@
+//! Authority contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
+contract Oracle is Owned {
+  
+  event Changed(uint224 current);
+    
+  struct Value {
+    uint32 timestamp;
+    uint224 value;
+  }
+    
+  function note(uint224 _value) only_owner {
+    if (data.value != _value) {
+      data.value = _value;
+      Changed(_value);
+    }
+		data.timestamp = uint32(now);
+    }
+    
+    function get() constant returns (uint224) {
+        return data.value;
+    }
+
+    function get_timestamp() constant returns (uint32) {
+        return data.timestamp;
+    }
+
+    Value public data;
+}
