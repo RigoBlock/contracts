e27d683330ca4aefb144ed28890f07e802335a24
escursionista <gabriele@rigoinvestment.com>
2017-04-01 12:28:05 +0200

    Update AssetCheckFace.sol

--- AssetCheck/AssetCheckFace.sol
+++ AssetCheck/AssetCheckFace.sol
@@ -6,4 +6,7 @@ pragma solidity ^0.4.10;
 
 contract AssetCheckFace {
       function assetCheck(address _asset) public constant returns (bool _approved) {}
+      function setApproval(address _asset, bool _status) onlyowner {}
+      function transferOwnership(address _owner) onlyowner {}
+      function OwnedVerifier(address _owner) {}
 }
