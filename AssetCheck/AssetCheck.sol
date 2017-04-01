4094ca495774256cd0f797645ed4e1006bde69a4
escursionista <gabriele@rigoinvestment.com>
2017-04-01 12:27:03 +0200

    Update AssetCheck.sol

--- AssetCheck/AssetCheck.sol
+++ AssetCheck/AssetCheck.sol
@@ -1,9 +1,25 @@
 //! Asset approved contract.
 //! By Gabriele Rigo (Rigo Investment), 2017.
 //! Released under the Apache Licence 2.
+//! Single assets can be limited.
 
 pragma solidity ^0.4.10;
 
-contract AssetCheck is AssetCheckFace {
+contract AssetCheck is Owned, AssetCheckFace {
+
       function assetCheck(address _asset) public constant returns (bool _approved) {}
+
+      function setApproval(address _asset, bool _status) onlyowner {
+            approved[_asset] = _status;
+      }
+
+      function transferOwnership(address _owner) onlyowner {
+            owner = _owner;
+      }
+
+      function OwnedVerifier(address _owner) {
+            owner = _owner;
+      }
+
+      mapping(address => bool) public approved;
 }
