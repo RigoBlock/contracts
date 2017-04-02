7e3741d5ab27142ddc7583a1821413a6d9b00357
escursionista <gabriele@rigoinvestment.com>
2017-04-02 18:26:12 +0200

    Update ERC20Face.sol

--- Dependencies/ERC20Face.sol
+++ Dependencies/ERC20Face.sol
@@ -8,8 +8,8 @@ contract ERC20Face {
 
 	//EVENTS
 	
-	event Transfer(address indexed _from, address indexed _to, uint256 _value) {}
-	event Approval(address indexed _owner, address indexed _spender, uint256 _value) {}
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
   
   	//METHODS
 	
