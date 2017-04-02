f5aa08079c92bc1425646239dc2e665640e90b01
escursionista <gabriele@rigoinvestment.com>
2017-04-02 22:18:36 +0200

    Update ERC23Face.sol

--- Dependencies/ERC23Face.sol
+++ Dependencies/ERC23Face.sol
@@ -6,15 +6,13 @@ contract ERC23 {
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
   //METHODS
-  
-  function balanceOf(address who) constant returns (uint);
-  function allowance(address owner, address spender) constant returns (uint);
 
   function transfer(address _to, uint256 _value) returns (bool success) {}
   function transfer(address _to, uint256 _value, bytes32 _data) returns (bool success) {}
   function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
   function approve(address _spender, uint256 _value) returns (bool success) {}
   
+  function totalSupply() constant returns (uint256 total) {}
   function balanceOf(address _owner) constant returns (uint256 balance) {}
   function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 }
