ecca37a7e05880718e942780e786e899c81f1c9c
escursionista <gabriele@rigoinvestment.com>
2017-04-02 21:24:23 +0200

    deposit/withdraw functions

--- Exchange/Exchange.sol
+++ Exchange/Exchange.sol
@@ -17,8 +17,32 @@ contract Exchange is ExchangeFace {
 
 	// METHODS
 
-	function deposit(address _who) payable {}	
-	function withdraw(uint value) returns (bool success) {}        
+	function deposit(address _who) payable {}
+	
+	function deposit(address token, uint256 amount) payable {
+    		if (token == address(0)) {
+      			tokens[address(0)][msg.sender] = safeAdd(tokens[address(0)][msg.sender], msg.value);
+    		} else {
+      			if (msg.value != 0) throw;
+      			tokens[token][msg.sender] = safeAdd(tokens[token][msg.sender], amount);
+      			if (!Token(token).transferFrom(msg.sender, this, amount)) throw;
+    		}
+    		Deposit(token, msg.sender, amount, tokens[token][msg.sender]);
+  	}
+	
+	function withdraw(uint value) returns (bool success) {}
+	
+	function withdraw(address token, uint256 amount) {
+    		if (tokens[token][msg.sender] < amount) throw;
+    		tokens[token][msg.sender] = safeSub(tokens[token][msg.sender], amount);
+    		if (token == address(0)) {
+      		if (!msg.sender.send(amount)) throw;
+    		} else {
+      		if (!Token(token).transfer(msg.sender, amount)) throw;
+    		}
+    		Withdraw(token, msg.sender, amount, tokens[token][msg.sender]);
+  	}
+  
 	function order(bool is_stable, uint32 adjustment, uint128 stake) payable {}
 	function cancel(uint32 id) {}
 	function finalize(uint24 id) {}
@@ -26,5 +50,9 @@ contract Exchange is ExchangeFace {
 	function best_adjustment(bool _is_stable) constant returns (uint32) {}
 	function best_adjustment_for(bool _is_stable, uint128 _stake) constant returns (uint32) {}
 	function deal_details(uint32 _id) constant returns (address stable, address leveraged, uint64 strike, uint128 stake, uint32 end_time) {}
-	function balance_of(address _who) constant returns (uint) {}
+	
+	function balance_of(address _who) constant returns (uint) {}	
+	function balanceOf(address token, address user) constant returns (uint256) {
+    		return tokens[token][user];
+  	}
 }
