5de4db249fb35e72be2ca0f960efcd8dc244fd1a
escursionista <gabriele@rigoinvestment.com>
2017-04-02 14:50:07 +0200

    Update Prices.sol

--- NAV/Prices.sol
+++ NAV/Prices.sol
@@ -1,1 +1,8 @@
+//! Prices contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Prices for NAV Estimate
 
+pragma solidity ^0.4.10;
+
+contract Prices is PricesFace {}
