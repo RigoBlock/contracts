972bdb874fb3fea32ef79d425cf1fc445ab970b9
escursionista <gabriele@rigoinvestment.com>
2017-04-02 14:50:37 +0200

    Update PricesFace.sol

--- NAV/PricesFace.sol
+++ NAV/PricesFace.sol
@@ -1,1 +1,8 @@
+//! Prices Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Prices for NAV Estimate
 
+pragma solidity ^0.4.10;
+
+contract PricesFace {}
