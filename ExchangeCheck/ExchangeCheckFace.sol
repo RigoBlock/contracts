6a1a81d8198e457dc6232607510aa4daded7d6e1
escursionista <gabriele@rigoinvestment.com>
2017-04-02 22:45:52 +0200

    Update ExchangeCheckFace.sol

--- ExchangeCheck/ExchangeCheckFace.sol
+++ ExchangeCheck/ExchangeCheckFace.sol
@@ -1,1 +1,12 @@
+//! Exchange approved contract.
+//! By Gabriele Rigo (Rigo Investment), 2017.
+//! Released under the Apache Licence 2.
 
+pragma solidity ^0.4.10;
+
+contract ExchangeCheckFace {
+      function exchangeCheck(address _exchange) public constant returns (bool _approved) {}
+      function setApproval(address _asset, bool _status) onlyowner {}
+      function transferOwnership(address _owner) onlyowner {}
+      function ownedVerifier(address _owner) {}
+}
