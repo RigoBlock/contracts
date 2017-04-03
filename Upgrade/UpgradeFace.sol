221474690d9a8cd03cfc357409bda389abe2d626
escursionista <gabriele@rigoinvestment.com>
2017-04-03 09:34:05 +0200

    Create UpgradeFace.sol

---
+++ Upgrade/UpgradeFace.sol
@@ -0,0 +1,14 @@
+//! Upgrade Interface contract.
+//! By Gabriele Rigo (Rigo Investment), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
+contract Upgrade {
+
+  function setUpgraded(uint done) restricted {}
+  function upgrade(address newAddress) restricted {}
+  
+  function getOwner() constant returns (address owner) {}
+  function getLastUpgrade() constant returns (uint lastDoneUpgrade) {}
+}
