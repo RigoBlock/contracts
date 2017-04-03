b4f7907aca2fa909dc00c47f4a4cb3e26dffc3a7
escursionista <gabriele@rigoinvestment.com>
2017-04-03 14:44:45 +0200

    Update Recovery.sol

--- Recovery/Recovery.sol
+++ Recovery/Recovery.sol
@@ -4,4 +4,25 @@
 
 pragma solidity ^0.4.10;
 
-contract Recovery is RecoveryFace {}
+contract Recovery is Owned, Multisig, RecoveryFace {
+  
+  modifier only_owner { assert(msg.sender == owner); _; }  
+  modifier only_recovery_address { assert(msg.sender == owner); _; }
+  modifier address_not_null(address addr) { assert(addr != 0); _; }
+  
+  function Recovery(address _recoverer) {
+    recoverer = _recoverer;
+  }
+  
+  function recoverOwner(address _newOwner) only_backup_owner address_not_null(_newOwner) {
+        owner = msg.sender;
+        recoveryOwner = _newOwner;
+  }
+ 
+  function setRecovery(address _newRecoverer) only_owner address_not_null(_newRecoverer) {
+    recoverer = _newRecoverer;
+  }
+
+ address public owner = msg.sender;
+ address public _recoverer;
+}
