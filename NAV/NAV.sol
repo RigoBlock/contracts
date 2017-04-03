c1e33212673c0784fbf56e2280cd29cf24ec6494
escursionista <gabriele@rigoinvestment.com>
2017-04-03 17:50:27 +0200

    Update NAV.sol

--- NAV/NAV.sol
+++ NAV/NAV.sol
@@ -4,4 +4,22 @@
 
 pragma solidity ^0.4.10;
 
-contract NAV is NAVFace {}
+contract NAV is NAVFace {
+
+  function estimateNAV() returns (uint) {
+    return estimateAUM();
+  }
+
+  function estimateAUM(address _drago) internal returns (uint256 aum) {
+    for (uint i = 0; i < numAssignedAssets; ++i) {
+      address _asset = address(asset(i));
+      ExchangFace exchange = ExchangeFace(_drago);
+      uint holdings = exchange.balanceOf(this); // Amount of asset base units this core holds
+      PricesFace prices = PricesFace(address(allAssets.prices(i)));
+      price = prices.getPrices(_asset);
+      aum = safeMul(holdings * prices);
+      nav = safeDiv( aum / totalSupply);
+      AumValue(holdings, price);
+    }  
+  } 
+}
