6f6d4e0c8db7dba757d7f518d19090e8b0ce62f3
escursionista <gabriele@rigoinvestment.com>
2017-04-03 17:09:17 +0200

    Update Prices.sol

--- NAV/Prices.sol
+++ NAV/Prices.sol
@@ -5,4 +5,47 @@
 
 pragma solidity ^0.4.10;
 
-contract Prices is PricesFace {}
+contract Prices is PricesFace {
+
+  struct Values {
+        uint32 timestamp;
+        uint224 values;
+  }
+  
+  event PricesUpdated(address indexed _asset, uint32 _timestamp, uint224 _price);
+  
+  function updatePrice(address[] _assets, uint224[] newPrices) /*only_owner*/ {}
+  
+  function getStatus(address _asset) constant data_initialised(_asset) returns (bool updated) {
+    return now - values[_asset].timestamp <= validity;
+  }
+    
+  function updatePrice(address[] _assets, uint224[] newPrices) /*only_owner*/ returns (uint224, uint32) {
+    for (uint i = 0; i < _assets.length; ++i) {
+      assert(data[_assets[i]].values != _values);
+      assert(data[_assets[i]].timestamp != now);
+      data[_assets[i]].values = newPrices[i];
+      data[_assets[i]].timestamp = uint32(now);
+      PriceUpdated(ofAssets[i], now, newPrices[i]);
+    }
+  }
+  
+  function getPrice(address[] _asset) constant returns (uint224) {
+    return value[_asset].price;
+  }
+  
+  function getTimestamp(address[] _asset) constant returns (uint32) {
+    return value[_asset].timestamp;
+  }
+  
+  function getValues(address _asset) constant returns (uint, uint224) {
+    return getValues;
+  }
+  
+  uint constant frequency = 120;
+  uint constant validity = 60;
+  Value public data;
+  address quoteAsset;
+  mapping (address => Values) data;
+}
+  
