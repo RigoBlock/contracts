a0e556fb1d3997a4679a1d4e0af093240acb8568
escursionista <gabriele@rigoinvestment.com>
2017-04-03 17:14:09 +0200

    Update PricesFace.sol

--- NAV/PricesFace.sol
+++ NAV/PricesFace.sol
@@ -7,11 +7,12 @@ pragma solidity ^0.4.10;
 
 contract PricesFace {
   
-  event PricesUpdated(address indexed _asset, uint224 _timestamp, uint _price);
+  event PricesUpdated(address indexed _asset, uint32 _timestamp, uint224 _price);
   
-  function updatePrice(address[] _assets, uint224[] newPrices) /*only_owner*/ {}
+  function updatePrice(address[] _assets, uint224[] newPrices) /*only_owner*/ returns (uint224, uint32) {}
 
-  function getPrice(address _asset) constant returns (uint224) {}
-  function getTimestamp(address _asset) constant returns (uint32) {}
-  function getData(address _asset) constant returns (uint, uint224) {}
+  function getPrice(address[] _asset) constant returns (uint224) {}
+  function getTimestamp(address[] _asset) constant returns (uint32) {}
+  function getValues(address _asset) constant returns (uint, uint224) {}
+  function getStatus(address _asset) constant data_initialised(_asset) returns (bool updated) {}
 }
