5327acb5bd2ebeec0a31d4a0a85d75e78991306a
escursionista <gabriele@rigoinvestment.com>
2017-04-06 22:22:15 +0200

    align to COP standards

--- RigoBlock/RigoTok.sol
+++ RigoBlock/RigoTok.sol
@@ -8,17 +8,17 @@ contract RigoTok is ERC20, SafeMath, RigoTokFace {
 
     event TokenMinted(address indexed recipient, uint amount);
 
-    modifier onlyMinter {
+    modifier only_minter {
         assert(msg.sender == minter);
         _;
     }
 
-    modifier onlyRigoblock {
+    modifier only_rigoblock {
         assert(msg.sender == rigoblock);
         _;
     }
 
-    modifier isLaterThan(uint x) {
+    modifier is_later_than(uint x) {
         assert(now > x);
         _;
     }
@@ -30,24 +30,24 @@ contract RigoTok is ERC20, SafeMath, RigoTokFace {
         endTime = setEndTime;
     }
 
-    function mintToken(address recipient, uint amount) external onlyMinter {
+    function mintToken(address recipient, uint amount) external only_minter {
         balances[recipient] = safeAdd(balances[recipient], amount);
         totalSupply = safeAdd(totalSupply, amount);
     }
 
-    function transfer(address recipient, uint amount) isLaterThan(endTime) returns (bool success) {
+    function transfer(address recipient, uint amount) is_later_than(endTime) returns (bool success) {
         return super.transfer(recipient, amount);
     }
 
-    function transferFrom(address sender, address recipient, uint amount) isLaterThan(endTime) returns (bool success) {
+    function transferFrom(address sender, address recipient, uint amount) is_later_than(endTime) returns (bool success) {
         return super.transferFrom(sender, recipient, amount);
     }
 
-    function changeMintingAddress(address newAddress) onlyRigoblock { 
+    function changeMintingAddress(address newAddress) only_rigoblock { 
         minter = newAddress; 
     }
 
-    function changeRigoblockAddress(address newAddress) onlyRigoblock {
+    function changeRigoblockAddress(address newAddress) only_rigoblock {
         rigoblock = newAddress;
     }
     
