11089f2cd5ba505d2ec7471211e60190c63c5980
escursionista <gabriele@rigoinvestment.com>
2017-04-06 19:49:45 +0200

    tabs

--- GabcoinFactory/GabcoinFactory.sol
+++ GabcoinFactory/GabcoinFactory.sol
@@ -50,23 +50,23 @@ contract Gabcoin is Owned, ERC20Face {
 
 contract GabcoinFactoryFace is Owned {
 
-    event GabcoinCreated(string _name, address _gabcoin, address _owner, uint _gabcoinID);
+	event GabcoinCreated(string _name, address _gabcoin, address _owner, uint _gabcoinID);
 
-    function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
-    function setFee(uint _fee) {}
-    function setBeneficiary(address _gabcoinDAO) {}
-    function drain() {}
-    function() {}
-    function buyGabcoin(address targetGabcoin) payable {}
-    function sellGabcoin(address targetGabcoin, uint256 amount) {}
-    function changeRatio(address targetGabcoin, uint256 _ratio) {}
-    function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
-    function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
-    function changeCoinator(address targetGabcoin, address _coinator) {}
+	function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
+	function setFee(uint _fee) {}
+	function setBeneficiary(address _gabcoinDAO) {}
+	function drain() {}
+	function() {}
+	function buyGabcoin(address targetGabcoin) payable {}
+	function sellGabcoin(address targetGabcoin, uint256 amount) {}
+	function changeRatio(address targetGabcoin, uint256 _ratio) {}
+	function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
+	function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
+	function changeCoinator(address targetGabcoin, address _coinator) {}
 
-    function getVersion() constant returns (string version) {}
-    function geeLastId() constant returns (uint _dragoID) {}
-    function getGavcoinDAO() constant returns (uint gabcoinDAO) {}
+	function getVersion() constant returns (string version) {}
+	function geeLastId() constant returns (uint _dragoID) {}
+	function getGavcoinDAO() constant returns (uint gabcoinDAO) {}
 }
 
 contract GabcoinFactory is Owned, GabcoinFactoryFace {
