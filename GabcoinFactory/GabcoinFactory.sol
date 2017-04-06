40a1c279d13e7590d268d92794d9265328a6c52d
escursionista <gabriele@rigoinvestment.com>
2017-04-06 19:53:19 +0200

    tabs

--- GabcoinFactory/GabcoinFactory.sol
+++ GabcoinFactory/GabcoinFactory.sol
@@ -54,19 +54,19 @@ contract GabcoinFactoryFace is Owned {
 
 	function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
 	function setFee(uint _fee) {}
-    function setBeneficiary(address _gabcoinDAO) {}
-    function drain() {}
-    function() {}
-    function buyGabcoin(address targetGabcoin) payable {}
-    function sellGabcoin(address targetGabcoin, uint256 amount) {}
-    function changeRatio(address targetGabcoin, uint256 _ratio) {}
-    function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
-    function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
-    function changeCoinator(address targetGabcoin, address _coinator) {}
+	function setBeneficiary(address _gabcoinDAO) {}
+	function drain() {}
+	function() {}
+	function buyGabcoin(address targetGabcoin) payable {}
+	function sellGabcoin(address targetGabcoin, uint256 amount) {}
+	function changeRatio(address targetGabcoin, uint256 _ratio) {}
+	function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
+	function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
+	function changeCoinator(address targetGabcoin, address _coinator) {}
 
-    function getVersion() constant returns (string version) {}
-    function geeLastId() constant returns (uint _dragoID) {}
-    function getGabcoinDao() constant returns (uint gabcoinDao) {}
+	function getVersion() constant returns (string version) {}
+	function geeLastId() constant returns (uint _dragoID) {}
+	function getGabcoinDao() constant returns (uint gabcoinDao) {}
 }
 
 contract GabcoinFactory is Owned, GabcoinFactoryFace {
