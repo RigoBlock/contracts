1668434508440217e97d1d8a70852f100591e390
escursionista <gabriele@rigoinvestment.com>
2017-04-06 13:26:18 +0200

    added dependencies

--- DragoFactory/DragoFactory.sol
+++ DragoFactory/DragoFactory.sol
@@ -4,12 +4,30 @@
 
 pragma solidity ^0.4.10;
 
+contract Owned {
+	event NewOwner(address indexed old, address indexed current);
+	function setOwner(address _new) {}
+	function getOwner() constant returns (address owner) {}
+}
+
 contract DragoRegistryFace {
 
-	function register(address _drago, uint _dragoID) {}
-    
-	function accountOf(uint _dragoID) constant returns (address) {}
+	event Registered(string indexed tla, uint indexed id, address addr, string name);
+	event Unregistered(string indexed tla, uint indexed id);
+	event MetaChanged(uint indexed id, bytes32 indexed key, bytes32 value);
+        
+	function register(address _drago, uint _dragoID) {}	
+	function register(address _addr, string _tla, uint _base, string _name) payable returns (bool) {}
+	function unregister(uint _id) only_owner {}
+	
+	function accountOf(uint _dragoID) constant returns (address) {}   
 	function dragoOf(address _drago) constant returns (uint) {}
+	function tokenCount() constant returns (uint) {}
+	function token(uint _id) constant returns (address addr, string tla, uint base, string name, address owner) {}
+	function fromAddress(address _addr) constant returns (uint id, string tla, uint base, string name, address owner) {}
+	function fromTLA(string _tla) constant returns (uint id, address addr, uint base, string name, address owner) {}
+	function meta(uint _id, bytes32 _key) constant returns (bytes32) {}
+	function setMeta(uint _id, bytes32 _key, bytes32 _value) only_token_owner(_id) {}
 }
 
 library DragoFactory is Dragowned, DragoFactoryFace {
