9157e898a332e6f19dfe9341a6354ad2e0892515
escursionista <gabriele@rigoinvestment.com>
2017-04-06 16:59:48 +0200

    remove modifier

--- Drago/DragoFace.sol
+++ Drago/DragoFace.sol
@@ -22,7 +22,7 @@ contract DragoFace is ERC20Face {
 	function changeDragator(address _dragator) {}
 	function setPrices(uint256 newSellPrice, uint256 newBuyPrice) {}
 	function DragoAdmin(string _dragoName,  string _dragoSymbol, address _dragowner) {}
-	function depositToExchange(address exchange, address _who) /*when_approved_exchange*/ payable returns(bool success) {}
+	function depositToExchange(address exchange, address _who) payable returns(bool success) {}
 	function withdrawFromExchange(address exchange, uint value) returns (bool success) {}
 	function placeOrderExchange(address exchange, bool is_stable, uint32 adjustment, uint128 stake) {}
 	function cancelOrderExchange(address exchange, uint32 id) {}
