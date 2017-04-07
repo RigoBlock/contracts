61a4603304ed5456bb713164041cf8bc8c91505d
escursionista <gabriele@rigoinvestment.com>
2017-04-07 17:59:22 +0200

    casper pre

--- GabcoinFactory/GabcoinFactoryFace.sol
+++ GabcoinFactory/GabcoinFactoryFace.sol
@@ -9,8 +9,8 @@ contract GabcoinFactoryFace is Owned {
 	event GabcoinCreated(string _name, address _gabcoin, address _owner, uint _gabcoinID);
 	//event GabcoinBought();
 	//event GabcoinSold();
-	//event PosDeposited(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _pos);
- 	//event PosWithdrew(uint deposit, address indexed msg.sender, address _pos);
+	//event CasperDeposit(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _casper);
+ 	//event CasperWithdraw(uint deposit, address indexed msg.sender, address _casper);
 
 	function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
 	function setFee(uint _fee) {}
@@ -19,8 +19,8 @@ contract GabcoinFactoryFace is Owned {
 	function() {}
 	function buyGabcoin(address targetGabcoin) payable {}
 	function sellGabcoin(address targetGabcoin, uint256 amount) {}
-	//function depositPos(address targetGabcoin, address _pos) {}
-	//function withdrawPos(address targetGabcoin, address _pos) {}
+	//function depositCasper(address targetGabcoin, address _casper) {}
+	//function withdrawCasper(address targetGabcoin, address _casper) {}
 	function changeRatio(address targetGabcoin, uint256 _ratio) {}
 	function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
 	function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
@@ -28,5 +28,6 @@ contract GabcoinFactoryFace is Owned {
 
 	function getVersion() constant returns (string version) {}
 	function geeLastId() constant returns (uint _dragoID) {}
-	function getGabcoinDao() constant returns (uint gabcoinDao) {}
+	function getGabcoinDao() constant returns (address gabcoinDao) {}
+	//function getCasper() constant returns (address casper) {}
 }
