aef3a6d917ccd93adbbc6251735c6e52a211d03e
escursionista <gabriele@rigoinvestment.com>
2017-04-07 17:51:14 +0200

    casper improvement

--- Gabcoin/GabcoinFace.sol
+++ Gabcoin/GabcoinFace.sol
@@ -14,7 +14,7 @@ contract ERC20Face is Owned {
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-  
+
 	function transfer(address _to, uint256 _value) returns (bool success) {}
 	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 	function approve(address _spender, uint256 _value) returns (bool success) {}
@@ -28,22 +28,20 @@ contract GabcoinFace is ERC20Face {
 
 	event Buy(address indexed from, address indexed to, uint256 indexed _amount, uint256 _revenue);
 	event Sell(address indexed from, address indexed to, uint256 indexed _amount,uint256 _revenue);
-	//event DepositPOS(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _pos);
- 	//event WithdrawPOS(uint deposit, address indexed msg.sender, address _pos);
+	//event DepositCasper(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _casper);
+ 	//event WithdrawCasper(uint deposit, address indexed msg.sender, address _casper);
 
-	
  	function Gabcoin(string _dragoName,  string _dragoSymbol) {}    
 	function() payable {}		
 	function buyGabcoin() payable returns (uint amount) {}	
 	function sellGabcoin(uint256 amount) returns (uint revenue, bool success) {}
-	//function depositPOS(address _validation, address _withdrawal, address _pos) returns (bool success) {}
-	//function withdrawPOS(uint _validatorIndex) only_approved_validator only_owner {}
+	//function depositCasper(address _validation, address _withdrawal, address _casper) returns (bool success) {}
+	//function withdrawCasper(uint _validatorIndex, address _casper) {}
 	function changeRatio(uint256 _ratio) {}	
 	function setTransactionFee(uint _transactionFee) {}	
 	function changeFeeCollector(address _feeCollector) {}	
 	function changeCoinator(address _coinator) {}
-	
-	
+
 	function balanceOf(address _from) constant returns (uint256 balance) {}
 	function getVersion() constant returns (string version) {}
 	function getName() constant returns (string name) {}
@@ -51,4 +49,5 @@ contract GabcoinFace is ERC20Face {
 	function getPrice() constant returns (uint256 price) {}
 	function getTransactionFee() constant returns (uint256 transactionFee) {}
 	function getFeeCollector() constant returns (address feeCollector) {}
+	function getCasper() constant returns (address _casper) {}
 }
