6dc62846af8b6622f6e3e31561b53f4d62f35b2d
escursionista <gabriele@rigoinvestment.com>
2017-04-07 16:59:09 +0200

    Proof of Stake functions

--- GabcoinFactory/GabcoinFactoryFace.sol
+++ GabcoinFactory/GabcoinFactoryFace.sol
@@ -7,20 +7,26 @@ pragma solidity ^0.4.10;
 contract GabcoinFactoryFace is Owned {
 
     event GabcoinCreated(string _name, address _gabcoin, address _owner, uint _gabcoinID);
+	//event GabcoinBought();
+	//event GabcoinSold();
+	//event PosDeposited(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _pos);
+ 	//event PosWithdrew(uint deposit, address indexed msg.sender, address _pos);
 
-    function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
-    function setFee(uint _fee) {}
-    function setBeneficiary(address _gabcoinDAO) {}
-    function drain() {}
-    function() {}
-    function buyGabcoin(address targetGabcoin) payable {}
-    function sellGabcoin(address targetGabcoin, uint256 amount) {}
-    function changeRatio(address targetGabcoin, uint256 _ratio) {}
-    function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
-    function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
-    function changeCoinator(address targetGabcoin, address _coinator) {}
+	function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
+	function setFee(uint _fee) {}
+	function setBeneficiary(address _gabcoinDAO) {}
+	function drain() {}
+	function() {}
+	function buyGabcoin(address targetGabcoin) payable {}
+	function sellGabcoin(address targetGabcoin, uint256 amount) {}
+	//function depositPos(address targetGabcoin, address _pos) {}
+	//function withdrawPos(address targetGabcoin, address _pos) {}
+	function changeRatio(address targetGabcoin, uint256 _ratio) {}
+	function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
+	function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
+	function changeCoinator(address targetGabcoin, address _coinator) {}
 
-    function getVersion() constant returns (string version) {}
-    function geeLastId() constant returns (uint _dragoID) {}
-    function getGabcoinDao() constant returns (uint gabcoinDao) {}
+	function getVersion() constant returns (string version) {}
+	function geeLastId() constant returns (uint _dragoID) {}
+	function getGabcoinDao() constant returns (uint gabcoinDao) {}
 }
