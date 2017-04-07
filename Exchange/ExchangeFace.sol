8142f9c06d7cbc3d9fa39159a1a1e085a44d4a8c
escursionista <gabriele@rigoinvestment.com>
2017-04-07 12:00:27 +0200

    improved exchange interface

--- Exchange/ExchangeFace.sol
+++ Exchange/ExchangeFace.sol
@@ -20,17 +20,20 @@ contract ExchangeFace {
     	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
     	event Deposit(address token, address user, uint amount, uint balance);
     	event Withdraw(address token, address user, uint amount, uint balance);
+	//event OrderUpdate(uint id);
+	
 	// METHODS
 
 	//function deposit(address _who) payable {}
 	function deposit(address token, uint256 amount) payable {}
 	//function withdraw(uint value) returns (bool success) {}
 	function withdraw(address token, uint256 amount) {}
-	function order(bool is_stable, uint32 adjustment, uint128 stake) payable {}
+	function order(bool is_stable, uint32 adjustment, uint128 stake) payable {}	//returns(uint id)
 	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
 	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
-    	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
-	function cancel(uint32 id) {}
+    	function trade(uint id, uint quantity) returns (bool) {}
+	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
+	function cancel(uint32 id) {}	//function cancel(uint id) returns (bool) {}
 	function finalize(uint24 id) {}
 	
 	function bestAdjustment(bool _is_stable) constant returns (uint32) {}
@@ -40,7 +43,11 @@ contract ExchangeFace {
 	function balanceOf(address token, address user) constant returns (uint) {}
 	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
     	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
-    
+    	
+	function getLastOrderId() constant returns (uint) {}
+	function isActive(uint id) constant returns (bool active) {}
+	function getOwner(uint id) constant returns (address owner) {}
+	//function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
     	//mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
     	//mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
     	//mapping (address => mapping (bytes32 => uint)) public orderFills;
