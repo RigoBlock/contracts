fa00782a3760c2d886b096e92c8c1ddd90061cd2
escursionista <gabriele@rigoinvestment.com>
2017-04-12 15:00:37 +0200

    Update Oracle.sol

--- Exchange/Oracle/Oracle.sol
+++ Exchange/Oracle/Oracle.sol
@@ -6,26 +6,28 @@ pragma solidity ^0.4.10;
 
 contract Oracle is Owned, OracleFace {
   
-  event Changed(uint224 current);
+    event Changed(uint224 current);
     
-  struct Value {
-    uint32 timestamp;
-    uint224 value;
-  }
+    struct Value {
+      uint32 timestamp;
+      uint224 value;
+    }
     
-  function note(uint224 _value) only_owner {
-    if (data.value != _value) {
-      data.value = _value;
-      Changed(_value);
+    function note(uint224 _value) only_owner {
+      if (data.value != _value) {
+       data.value = _value;
+       Changed(_value);
+      }
+	    data.timestamp = uint32(now);
     }
-	data.timestamp = uint32(now);
-    }
+    
+    function updatePrice() {}
     
     function get() constant returns (uint224) {
         return data.value;
     }
 
-    function get_timestamp() constant returns (uint32) {
+    function getTimestamp() constant returns (uint32) {
         return data.timestamp;
     }
 
