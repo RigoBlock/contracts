24645d0489301cbd0da07e924175161a2b628a21
escursionista <gabriele@rigoinvestment.com>
2017-04-12 14:18:48 +0200

    tabs

--- Exchange/Exchange.sol
+++ Exchange/Exchange.sol
@@ -58,11 +58,11 @@ contract ExchangeFace {
 	//event OrderCancelled(uint32 indexed id, address indexed who, uint128 stake);
 	//event DealFinalized(uint32 indexed id, address indexed stable, address indexed leveraged, uint64 price);
 
-    event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
-    event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
-    event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
-    event Deposit(address token, address user, uint amount, uint balance);
-    event Withdraw(address token, address user, uint amount, uint balance);
+    	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+    	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
+    	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
+    	event Deposit(address token, address user, uint amount, uint balance);
+    	event Withdraw(address token, address user, uint amount, uint balance);
 	//event OrderUpdate(uint id);
 	
 	// METHODS
@@ -72,7 +72,7 @@ contract ExchangeFace {
 	function order(bool is_stable, uint32 adjustment, uint128 stake) payable {}	//returns(uint id)
 	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
 	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
-    function trade(uint id, uint quantity) returns (bool) {}
+    	function trade(uint id, uint quantity) returns (bool) {}
 	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
 	function cancel(uint32 id) {}	//function cancel(uint id) returns (bool) {}
 	function finalize(uint24 id) {}
@@ -81,32 +81,32 @@ contract ExchangeFace {
 	function balanceOf(address _who) constant returns (uint256) {}
 	function balanceOf(address token, address user) constant returns (uint256) {}
 	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
-    function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
+    	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
     	
 	function getLastOrderId() constant returns (uint) {}
 	function isActive(uint id) constant returns (bool active) {}
 	function getOwner(uint id) constant returns (address owner) {}
 	//function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
-    //mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
-    //mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
-    //mapping (address => mapping (bytes32 => uint)) public orderFills;
+    	//mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
+    	//mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
+    	//mapping (address => mapping (bytes32 => uint)) public orderFills;
 }
 
 contract Exchange is ExchangeFace, SafeMath, Owned {
     
-    struct CfdInfo {
-        address cfd;
-        //CFD sell_which_cfd;   //CFD cfd;
+    	struct CfdInfo {
+      	  	address cfd;
+      	  	//CFD sell_which_cfd;   //CFD cfd;
 		address who;
 		bool is_stable;
 		uint32 adjustment;	// billionths by which to adjust start-price
 		uint128 stake;
-        uint32 id;
+        	uint32 id;
 		uint32 prev_id;		// a linked ring
 		uint32 next_id;		// a linked ring
 	}
 
-    struct Receipt {
+    	struct Receipt {
 		uint units;
 		uint32 activation;
 	}
@@ -136,11 +136,11 @@ contract Exchange is ExchangeFace, SafeMath, Owned {
         	bytes32 hash = sha256(msg.sender);
         	assert(ecrecover(hash, v, r, s) == signer);
         	_;
-    }
+    	}
     
-    modifier only_owner { if (msg.sender != owner) return; _; }
+    	modifier only_owner { if (msg.sender != owner) return; _; }
     
-    modifier margin_ok(uint x) { if (accounts[msg.sender].balance < x) return; _; }
+    	modifier margin_ok(uint x) { if (accounts[msg.sender].balance < x) return; _; }
 
 	//function deposit(address _who) payable {}
 	function deposit(address token, uint256 amount) payable {
