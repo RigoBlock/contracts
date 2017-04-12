ec521185da655410e3c0fd6c6c34810ef1a2a7ba
escursionista <gabriele@rigoinvestment.com>
2017-04-12 13:17:34 +0200

    Update CFDFace.sol

--- Exchange/CFD/CFDFace.sol
+++ Exchange/CFD/CFDFace.sol
@@ -7,16 +7,23 @@ pragma solidity ^0.4.10;
 contract CFDFace {
 
   // EVENTS
-
+	
+	event Deposit(address indexed who, uint value);
+	event Withdraw(address indexed who, uint value);
 	event OrderPlaced(uint32 indexed id, address indexed who, bool indexed is_stable, uint32 adjustment, uint128 stake);
 	event OrderMatched(uint32 indexed id, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
 	event OrderCancelled(uint32 indexed id, address indexed who, uint128 stake);
 	event DealFinalized(uint32 indexed id, address indexed stable, address indexed leveraged, uint64 price);
 
 	// METHODS
-      
+	
+	function deposit(address _who) payable {}
+	function withdraw(uint value) returns (bool success) {}
+	function orderExchange(bool is_stable, uint32 adjustment, uint128 stake) {}
 	function order(bool is_stable, uint32 adjustment, uint128 stake) payable {}
+	function cancelExchange(uint32 id) {}
 	function cancel(uint32 id) {}
+	function finalizeExchange(uint24 id) {}
 	function finalize(uint24 id) {}
 	
 	function best_adjustment(bool _is_stable) constant returns (uint32) {}
