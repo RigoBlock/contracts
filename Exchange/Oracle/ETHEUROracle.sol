e8bbdfdf57db3e5bed7d19874d28dba01d2a710d
escursionista <gabriele@rigoinvestment.com>
2017-04-12 14:46:12 +0200

    Update ETHEUROracle.sol

--- Exchange/Oracle/ETHEUROracle.sol
+++ Exchange/Oracle/ETHEUROracle.sol
@@ -4,11 +4,63 @@
 
 pragma solidity ^0.4.10;
 
+contract Owned {
+    
+    event NewOwner(address indexed old, address indexed current);
+    
+    modifier only_owner { if (msg.sender != owner) return; _; }
+    
+    function setOwner(address _new) only_owner {
+        owner = _new;
+        NewOwner(owner, _new);
+    }
+    
+    address public owner = msg.sender;
+}
+
+contract OracleFace is Owned {
+  
+  event Changed(uint224 current);
+  
+  function note(uint224 _value) only_owner {}
+    
+  function get() constant returns (uint224) {}
+  function getTimestamp() constant returns (uint32) {}
+}
+
+contract Oracle is Owned, OracleFace {
+  
+    event Changed(uint224 current);
+    
+    struct Value {
+      uint32 timestamp;
+      uint224 value;
+    }
+    
+    function note(uint224 _value) only_owner {
+      if (data.value != _value) {
+       data.value = _value;
+       Changed(_value);
+      }
+	    data.timestamp = uint32(now);
+    }  
+    
+    function get() constant returns (uint224) {
+        return data.value;
+    }
+
+    function getTimestamp() constant returns (uint32) {
+        return data.timestamp;
+    }
+
+    Value public data;
+}
+
 contract ETHEUROracle.sol is Oracle {
 
-  function updatePrice() onlyOwner {
-    feed = json(https://api.kraken.com/0/public/Ticker?pair=ETHEUR).result.XETHZEUR.c.0;
-    newPrice = safeMul( feed * 10 ** 18 );
-    note(newPrice);
-  }
+    function updatePrice() onlyOwner {
+       feed = json(https://api.kraken.com/0/public/Ticker?pair=ETHEUR).result.XETHZEUR.c.0;
+       newPrice = safeMul( feed * 10 ** 18 );
+       note(newPrice);
+    }
 }
