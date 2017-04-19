cc53b9eabbe4db96ae6e565c7b2d3d57a94467ab
escursionista <gabriele@rigoinvestment.com>
2017-04-19 16:23:32 +0200

    Create ErcExchangeFace.sol

---
+++ Exchange/ErcExchangeFace.sol
@@ -0,0 +1,33 @@
+//! ERC20 Exchange Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
+contract ErcExchangeFace {
+
+	// EVENTS
+
+  event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+  event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
+  event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
+  event Deposit(address token, address user, uint amount, uint balance);
+  event Withdraw(address token, address user, uint amount, uint balance);
+
+	// METHODS
+
+	function deposit(address token, uint256 amount) payable {}
+	function withdraw(address token, uint256 amount) {}
+	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
+	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
+	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
+
+	function balanceOf(address _who) constant returns (uint256) {}
+	function balanceOf(address token, address user) constant returns (uint256) {}
+	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
+  function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
+    	
+	function getLastOrderId() constant returns (uint) {}
+	function isActive(uint id) constant returns (bool active) {}
+	function getOwner(uint id) constant returns (address owner) {}
+}
