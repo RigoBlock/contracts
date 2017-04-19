f5f34bf4e146adfb39c3a23cb51751c8b529cee1
escursionista <gabriele@rigoinvestment.com>
2017-04-19 16:14:58 +0200

    Update ErcExchange.sol

--- Exchange/ErcExchange.sol
+++ Exchange/ErcExchange.sol
@@ -38,7 +38,7 @@ contract SafeMath {
 }
 
 contract AccountLevels {
-	    function accountLevel(address user) constant returns(uint) {}
+	function accountLevel(address user) constant returns(uint) {}
 }
 
 contract ERC20 {
@@ -46,7 +46,7 @@ contract ERC20 {
 	    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 	    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 	
-  	  function transfer(address _to, uint256 _value) returns (bool success) {}
+	    function transfer(address _to, uint256 _value) returns (bool success) {}
 	    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 	    function approve(address _spender, uint256 _value) returns (bool success) {}
 
@@ -57,35 +57,35 @@ contract ERC20 {
 
 contract ErcExchangeFace {
 
-	    // EVENTS
+	// EVENTS
 
-    	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
     	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
     	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
     	event Deposit(address token, address user, uint amount, uint balance);
     	event Withdraw(address token, address user, uint amount, uint balance);
 
-	    // METHODS
+	// METHODS
 
-	    function deposit(address token, uint256 amount) payable {}
-	    function withdraw(address token, uint256 amount) {}
-	    function orderCFD(bool is_stable, uint32 adjustment, uint128 stake) payable {}	//returns(uint id)
-	    function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
-	    function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
-	    function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
-	    function cancel(uint32 id) {}	//function cancel(uint id) returns (bool) {}
-	    function finalize(uint24 id) {}
-	    function moveOrder(uint id, uint quantity) returns (bool) {}
+	function deposit(address token, uint256 amount) payable {}
+	function withdraw(address token, uint256 amount) {}
+	function orderCFD(bool is_stable, uint32 adjustment, uint128 stake) payable {}	//returns(uint id)
+	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
+	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
+	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
+	function cancel(uint32 id) {}	//function cancel(uint id) returns (bool) {}
+	function finalize(uint24 id) {}
+	function moveOrder(uint id, uint quantity) returns (bool) {}
 	
-	    function balanceOf(address _who) constant returns (uint256) {}
-	    function balanceOf(address token, address user) constant returns (uint256) {}
-	    function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
+	function balanceOf(address _who) constant returns (uint256) {}
+	function balanceOf(address token, address user) constant returns (uint256) {}
+	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
     	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
     	
-	    function getLastOrderId() constant returns (uint) {}
-	    function isActive(uint id) constant returns (bool active) {}
-	    function getOwner(uint id) constant returns (address owner) {}
-	    //function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
+	function getLastOrderId() constant returns (uint) {}
+	function isActive(uint id) constant returns (bool active) {}
+	function getOwner(uint id) constant returns (address owner) {}
+	//function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
     	//mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
     	//mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
     	//mapping (address => mapping (bytes32 => uint)) public orderFills;
@@ -94,36 +94,38 @@ contract ErcExchangeFace {
 contract ErcExchange is ErcExchangeFace, SafeMath, Owned {
 
     	struct Receipt {
-		      uint units;
-		      uint32 activation;
-	    }
+		uint units;
+		uint32 activation;
+	}
 	
-	    struct Account {
-		      uint balance;
-		      mapping (uint => Receipt) receipt;
-		      mapping (address => uint) allowanceOf;
-	    }
+	struct Account {
+		uint balance;
+		mapping (uint => Receipt) receipt;
+		mapping (address => uint) allowanceOf;
+	}
 
-	    // EVENTS
+	// EVENTS
 
-	    event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
-  	  event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
-  	  event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
-  	  event Deposit(address token, address user, uint amount, uint balance);
-  	  event Withdraw(address token, address user, uint amount, uint balance);
+	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
+	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
+	event Deposit(address token, address user, uint amount, uint balance);
+	event Withdraw(address token, address user, uint amount, uint balance);
 	
-	    // METHODS
+	// METHODS
 	
-	    modifier is_signer_signature(uint8 v, bytes32 r, bytes32 s) {
+	modifier is_signer_signature(uint8 v, bytes32 r, bytes32 s) {
         	bytes32 hash = sha256(msg.sender);
         	assert(ecrecover(hash, v, r, s) == signer);
         	_;
     	}
     
     	modifier only_owner { if (msg.sender != owner) return; _; }
+	        modifier margin_ok(uint x) { if (accounts[msg.sender].balance < x) return; _; }
 
-	    function deposit(address token, uint256 amount) payable {
-          //remember to call Token(address).approve(this, amount) or this contract will not be able to do the transfer on your behalf.
+
+	function deposit(address token, uint256 amount) payable {
+		//remember to call Token(address).approve(this, amount) or this contract will not be able to do the transfer on your behalf.
         	if (token == address(0)) {
             		tokens[address(0)][msg.sender] = safeAdd(tokens[address(0)][msg.sender], msg.value);
             		accounts[msg.sender].balance = safeAdd(accounts[msg.sender].balance, msg.value);
@@ -134,9 +136,9 @@ contract ErcExchange is ErcExchangeFace, SafeMath, Owned {
             		if (!ERC20(token).transferFrom(msg.sender, this, amount)) throw;
         	}
        		Deposit(token, msg.sender, amount, tokens[token][msg.sender]);
-	    }
+	}
 	
-	    function withdraw(address token, uint256 amount) margin_ok(amount) {
+	function withdraw(address token, uint256 amount) margin_ok(amount) {
         	if (tokens[token][msg.sender] < amount) throw;
         	tokens[token][msg.sender] = safeSub(tokens[token][msg.sender], amount);
         	if (token == address(0)) {
@@ -147,13 +149,13 @@ contract ErcExchange is ErcExchangeFace, SafeMath, Owned {
         	Withdraw(token, msg.sender, amount, tokens[token][msg.sender]);
     	}
 	
-	    function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {
+	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {
     	  	bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
     		  orders[msg.sender][hash] = true;
     		  Order(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, msg.sender);
-  	  }
+	}
 
-	    function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {
+	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {
     	  	//amount is in amountGet terms
     		  bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
     		  if (!(
@@ -166,40 +168,40 @@ contract ErcExchange is ErcExchangeFace, SafeMath, Owned {
     		  tradeBalances(tokenGet, amountGet, tokenGive, amountGive, user, amount);
     		  orderFills[user][hash] = safeAdd(orderFills[user][hash], amount);
     		  Trade(tokenGet, amount, tokenGive, amountGive * amount / amountGet, user, msg.sender);
-  	  }
+	}
 	
-	    function tradeBalances(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address user, uint amount) internal {
-    	  	uint feeMakeXfer = safeMul(amount, feeMake) / (1 ether);
-    		  uint feeTakeXfer = safeMul(amount, feeTake) / (1 ether);
-    		  uint feeRebateXfer = 0;
-    		  if (accountLevelsAddr != 0x0) {
+	function tradeBalances(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address user, uint amount) internal {
+		uint feeMakeXfer = safeMul(amount, feeMake) / (1 ether);
+		uint feeTakeXfer = safeMul(amount, feeTake) / (1 ether);
+		uint feeRebateXfer = 0;
+		if (accountLevelsAddr != 0x0) {
       		  	uint accountLevel = AccountLevels(accountLevelsAddr).accountLevel(user);
-      			  if (accountLevel==1) feeRebateXfer = safeMul(amount, feeRebate) / (1 ether);
-      			  if (accountLevel==2) feeRebateXfer = feeTakeXfer;
-    		  }
-    		  tokens[tokenGet][msg.sender] = safeSub(tokens[tokenGet][msg.sender], safeAdd(amount, feeTakeXfer));
-    		  tokens[tokenGet][user] = safeAdd(tokens[tokenGet][user], safeSub(safeAdd(amount, feeRebateXfer), feeMakeXfer));
-    		  tokens[tokenGet][feeAccount] = safeAdd(tokens[tokenGet][feeAccount], safeSub(safeAdd(feeMakeXfer, feeTakeXfer), feeRebateXfer));
-    		  tokens[tokenGive][user] = safeSub(tokens[tokenGive][user], safeMul(amountGive, amount) / amountGet);
-    		  tokens[tokenGive][msg.sender] = safeAdd(tokens[tokenGive][msg.sender], safeMul(amountGive, amount) / amountGet);
-  	  }
+			if (accountLevel==1) feeRebateXfer = safeMul(amount, feeRebate) / (1 ether);
+			if (accountLevel==2) feeRebateXfer = feeTakeXfer;
+		}
+		tokens[tokenGet][msg.sender] = safeSub(tokens[tokenGet][msg.sender], safeAdd(amount, feeTakeXfer));
+		tokens[tokenGet][user] = safeAdd(tokens[tokenGet][user], safeSub(safeAdd(amount, feeRebateXfer), feeMakeXfer));
+		tokens[tokenGet][feeAccount] = safeAdd(tokens[tokenGet][feeAccount], safeSub(safeAdd(feeMakeXfer, feeTakeXfer), feeRebateXfer));
+		tokens[tokenGive][user] = safeSub(tokens[tokenGive][user], safeMul(amountGive, amount) / amountGet);
+		tokens[tokenGive][msg.sender] = safeAdd(tokens[tokenGive][msg.sender], safeMul(amountGive, amount) / amountGet);
+	}
 	
-	    function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {
-   		    bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
-    		  if (!(orders[msg.sender][hash] || ecrecover(sha3("\x19Ethereum Signed Message:\n32", hash),v,r,s) == msg.sender)) throw;
-    		  orderFills[msg.sender][hash] = amountGet;
-    		  Cancel(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, msg.sender, v, r, s);
-  	  }
+	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {
+		bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
+		if (!(orders[msg.sender][hash] || ecrecover(sha3("\x19Ethereum Signed Message:\n32", hash),v,r,s) == msg.sender)) throw;
+		orderFills[msg.sender][hash] = amountGet;
+		Cancel(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, msg.sender, v, r, s);
+	}
 	
-	    function balanceOf(address _who) constant returns (uint256) {
+	function balanceOf(address _who) constant returns (uint256) {
 	        return tokens[address(0)][msg.sender];
-	    }        
+	}        
 	
-	    function balanceOf(address token, address user) constant returns (uint256) {
+	function balanceOf(address token, address user) constant returns (uint256) {
     	  	return tokens[token][user];
-  	  }
+	}
 	
-	    function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {
+	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {
     	  	bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
     	  	if (!(
       	  		(orders[user][hash] || ecrecover(sha3("\x19Ethereum Signed Message:\n32", hash),v,r,s) == user) &&
@@ -209,21 +211,21 @@ contract ErcExchange is ErcExchangeFace, SafeMath, Owned {
     		  uint available2 = safeMul(tokens[tokenGive][user], amountGet) / amountGive;
     		  if (available1<available2) return available1;
     		  return available2;
-  	  }
+	}
 	
-	    function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {
+	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {
     	  	bytes32 hash = sha256(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce);
     		  return orderFills[user][hash];
-  	  }
+	}
 	
-	    mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
-  	  mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
-  	  mapping (address => mapping (bytes32 => uint)) public orderFills;
-  	  mapping (address => Account) accounts;
-  	  uint public feeMake; //percentage times (1 ether)
-  	  uint public feeTake; //percentage times (1 ether)
-  	  uint public feeRebate;
-  	  address public feeAccount; //the account that will receive fees
-  	  address public accountLevelsAddr;
-	    address public signer = msg.sender;
+	mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
+	mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
+	mapping (address => mapping (bytes32 => uint)) public orderFills;
+	mapping (address => Account) accounts;
+	uint public feeMake; //percentage times (1 ether)
+	uint public feeTake; //percentage times (1 ether)
+	uint public feeRebate;
+	address public feeAccount; //the account that will receive fees
+	address public accountLevelsAddr;
+	address public signer = msg.sender;
 }
