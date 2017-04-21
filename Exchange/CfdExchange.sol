e0d3826fff53fae939a3dea44004583314c188b9
escursionista <gabriele@rigoinvestment.com>
2017-04-21 17:43:23 +0200

    Update CfdExchange.sol

--- Exchange/CfdExchange.sol
+++ Exchange/CfdExchange.sol
@@ -71,6 +71,8 @@ contract CfdExchangeFace {
 
 	// EVENTS
 	
+	event Deposit(address token, address user, uint amount, uint balance);
+  	event Withdraw(address token, address user, uint amount, uint balance);
 	event OrderPlaced(address indexed cfd, address indexed who, bool indexed is_stable, uint32 adjustment, uint128 stake);
 	event OrderMatched(address indexed cfd, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
 	event OrderCancelled(address indexed cfd, address indexed who, uint128 stake);
@@ -80,10 +82,10 @@ contract CfdExchangeFace {
 
 	function deposit() payable {}
 	function withdraw(uint256 amount) {}
-	function orderCFD(bool is_stable, uint32 adjustment, uint128 stake) payable {}	//returns(uint id)
-	function cancel(uint32 id) {}	//function cancel(uint id) returns (bool) {}
-	function finalize(uint24 id) {}
-	function moveOrder(uint id, uint quantity) returns (bool) {}
+	function orderCFD(address _cfd, bool is_stable, uint32 adjustment, uint128 stake) {}	//returns(uint id)
+	function cancel(address _cfd, uint32 id) {}	//function cancel(uint id) returns (bool) {}
+	function finalize(address _cfd, uint24 id) {}
+	function moveOrder(address _cfd, uint24 id, bool is_stable, uint32 adjustment) returns (bool) {}
 	
 	function balanceOf(address _who) constant returns (uint256) {}
 	function getLastOrderId() constant returns (uint) {}
@@ -143,8 +145,10 @@ contract CfdExchange is CfdExchangeFace, SafeMath, Owned {
 		OrderPlaced(_cfd, msg.sender, is_stable, adjustment, stake);
 	}
 	
-	function moveOrder(address _cfd, uint32 id, bool is_stable, uint32 adjustment, uint128 stake) returns (bool) {
+	function moveOrder(address _cfd, uint24 id, bool is_stable, uint32 adjustment) returns (bool) {
+		CFD cfd = CFD(_cfd);
 		cancel(_cfd, id);
+		var stake = cfd.orderDetails(id);
 		orderCFD(_cfd, is_stable, adjustment, stake);
 	}
   
