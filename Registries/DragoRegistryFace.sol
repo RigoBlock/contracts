0f8ca859bf9f5fe93c4ccccf042f49414d91ab4a
escursionista <gabriele@rigoinvestment.com>
2017-04-26 21:32:57 +0200

    Update DragoRegistryFace.sol

--- Registries/DragoRegistryFace.sol
+++ Registries/DragoRegistryFace.sol
@@ -6,20 +6,27 @@ pragma solidity ^0.4.10;
 
 contract DragoRegistryFace {
 
+	//EVENTS
+
 	event Registered(string indexed tla, uint indexed id, address addr, string name);
 	event Unregistered(string indexed tla, uint indexed id);
 	event MetaChanged(uint indexed id, bytes32 indexed key, bytes32 value);
+	
+	// METHODS
         
 	function register(address _drago, uint _dragoID) {}	
-	function register(address _addr, string _tla, uint _base, string _name) payable returns (bool) {}
+	function register(address _drago, string _symbol, uint _base, string _name) payable returns (bool) {}
 	function unregister(uint _id) {}
+	function setMeta(uint _id, bytes32 _key, bytes32 _value) {}
+	function setFee(uint _fee) {}
+	function drain() {}
 	
 	function accountOf(uint _dragoID) constant returns (address) {}   
 	function dragoOf(address _drago) constant returns (uint) {}
-	function tokenCount() constant returns (uint) {}
-	function token(uint _id) constant returns (address addr, string tla, uint base, string name, address owner) {}
-	function fromAddress(address _addr) constant returns (uint id, string tla, uint base, string name, address owner) {}
-	function fromTLA(string _tla) constant returns (uint id, address addr, uint base, string name, address owner) {}
+	function dragoCount() constant returns (uint) {}
+	function drago(uint _id) constant returns (address drago, string symbol, uint base, string name, address owner) {}
+	function fromAddress(address _drago) constant returns (uint id, string symbol, uint base, string name, address owner) {}
+	function fromSymbol(string _symbol) constant returns (uint id, address drago, uint base, string name, address owner) {}
+	function fromName(string _name) constant returns (uint id, string symbol, address drago, uint base, address owner) {}
 	function meta(uint _id, bytes32 _key) constant returns (bytes32) {}
-	function setMeta(uint _id, bytes32 _key, bytes32 _value) {}
 }
