5b9cebf0ed16e45cbe3b899e87df7a870df79244
escursionista <gabriele@rigoinvestment.com>
2017-04-26 17:28:17 +0200

    pragma

--- Drago/DragoAdmin/DragoAdminFace.sol
+++ Drago/DragoAdmin/DragoAdminFace.sol
@@ -1,3 +1,9 @@
+//! Drago Admin Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
 contract DragoAdminFace {
 	function buyDrago(address targetDrago) payable {}
 	function sellDrago(address targetDrago, uint256 amount) {}
