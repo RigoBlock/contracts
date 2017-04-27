20b3a245779cd48558ebc8e6ba13114d70748498
escursionista <gabriele@rigoinvestment.com>
2017-04-28 00:28:00 +0200

    Update DragoAdminFace.sol

--- Drago/DragoAdmin/DragoAdminFace.sol
+++ Drago/DragoAdmin/DragoAdminFace.sol
@@ -5,8 +5,25 @@
 pragma solidity ^0.4.10;
 
 contract DragoAdminFace {
-	function buyDrago(address targetDrago) payable {}
-	function sellDrago(address targetDrago, uint256 amount) {}
+
+	// EVENTS
+
+	event Buy(address indexed from, address indexed to, uint256 indexed _amount, uint256 _revenue);
+	event Sell(address indexed from, address indexed to, uint256 indexed _amount, uint256 _revenue);
+	event NAV(uint sellPrice, uint buyPrice);
+	event DepositExchange(uint value, uint256 indexed _amount, address indexed who, address token , address indexed _exchange);
+	event DepositCFDExchange(uint , uint256 indexed _amount, address indexed who, address , address indexed _cfdExchange);
+	event WithdrawExchange(uint , uint256 indexed _amount, address indexed who, address , address indexed _cfdExchange);
+	event WithdrawCFDExchange(uint , uint256 indexed _amount, address indexed who, address , address indexed _cfdExchange);
+	event OrderCFD(address indexed _cfdExchange, address indexed _cfd);
+	event CancelCFD(address indexed _cfdExchange, address indexed _cfd);
+	event FinalizeCFD(address indexed _cfdExchange, address indexed _cfd);
+	
+	// METHODS
+	
+	function buyDrago(address targetDrago) payable returns (uint amount) {}
+	function sellDrago(address targetDrago, uint256 amount) returns (uint revenue) {}
+	function setDragoPrice(address _targetDrago, uint _sellPrice, uint _buyPrice) {}
 	function changeRatio(address _targetDrago, uint256 _ratio) {}
 	function setTransactionFee(address _targetDrago, uint _transactionFee) {}
 	function changeFeeCollector(address _targetDrago, address _feeCollector) {}
@@ -16,8 +33,9 @@ contract DragoAdminFace {
 	function withdrawFromExchange(address targetDrago, address exchange, address token, uint256 value) returns (bool) {}
 	function withdrawFromCFDExchange(address _targetDrago, address _cfdExchange, uint amount) returns(bool) {}
 	function placeOrderExchange() {}
+	function placeTradeExchange() {}
 	function placeOrderCFDExchange(address _targetDrago, address _cfdExchange, address _cfd, bool is_stable, uint32 adjustment, uint128 stake) {}
 	function cancelOrderExchange() {}
 	function cancelOrderCFDExchange(address targetDrago, address _cfdExchange, address _cfd, uint32 id) {}
 	function finalizedDealExchange(address targetDrago, address exchange, uint24 id) {}
-}  
+} 
