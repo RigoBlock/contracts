56342238d8986d74e363ba3244f006fc66d0e512
escursionista <gabriele@rigoinvestment.com>
2017-04-29 23:57:55 +0200

    improved efficiency

--- DragoFactory/DragoFactory.sol
+++ DragoFactory/DragoFactory.sol
@@ -16,19 +16,23 @@ contract DragoRegistry {
 	
 	// METHODS
         
-	function register(address _drago, string _name, string _symbol, uint _dragoID) payable returns (bool) {}
-	function registerAs(address _drago, string _name, string _symbol, uint _dragoID, address _owner) payable returns (bool) {}
+	function register(address _drago, string _name, string _symbol, uint _dragoID, address _group) payable returns (bool) {}
+	function registerAs(address _drago, string _name, string _symbol, uint _dragoID, address _group, address _owner) payable returns (bool) {}
 	function unregister(uint _id) {}
 	function setMeta(uint _id, bytes32 _key, bytes32 _value) {}
 	function setFee(uint _fee) {}
+	function upgrade(address _newAddress) payable {}
+	function setUpgraded(uint _version) {}
 	function drain() {}
+	function kill() {}
 	
 	function dragoCount() constant returns (uint) {}
-	function drago(uint _id) constant returns (address drago, string name, string symbol, uint dragoID, address owner) {}
-	function fromAddress(address _drago) constant returns (uint id, string name, string symbol, uint dragoID, address owner) {}
-	function fromSymbol(string _symbol) constant returns (uint id, address drago, string name, uint dragoID, address owner) {}
-	function fromName(string _name) constant returns (uint id, address drago, string symbol, uint dragoID, address owner) {}
+	function drago(uint _id) constant returns (address drago, string name, string symbol, uint dragoID, address owner, address group) {}
+	function fromAddress(address _drago) constant returns (uint id, string name, string symbol, uint dragoID, address owner, address group) {}
+	function fromSymbol(string _symbol) constant returns (uint id, address drago, string name, uint dragoID, address owner, address group) {}
+	function fromName(string _name) constant returns (uint id, address drago, string symbol, uint dragoID, address owner, address group) {}
 	function meta(uint _id, bytes32 _key) constant returns (bytes32) {}
+	function getGroups(address _group) constant returns (address[]) {}
 }
 
 contract DragoFactoryFace {
@@ -56,9 +60,8 @@ contract DragoFactory is Owned, DragoFactoryFace {
     struct Data {
 	    uint fee;
 	    address dragoRegistry;
-	    address[] newDragos;
 	    uint nextDragoID;
-	    mapping(address => address[]) created;
+	    mapping(address => address[]) newDragos;
 	}
 
 	event DragoCreated(string indexed _name, string _symbol, address indexed _drago, address indexed _owner, uint _dragoID);
@@ -71,11 +74,10 @@ contract DragoFactory is Owned, DragoFactoryFace {
 	function createDrago(string _name, string _symbol) when_fee_paid returns (bool success) {
 		++data.nextDragoID;
 		uint dragoID = data.nextDragoID;
-		Drago newDrago = (new Drago(_name, _symbol, dragoID));
-		data.newDragos.push(address(newDrago));
-		data.created[msg.sender].push(address(newDrago));
-		newDrago.setOwner(msg.sender);  //owner is msg.sender
+		Drago newDrago = new Drago(_name, _symbol, dragoID);
 		address drago = address(newDrago);
+		data.newDragos[msg.sender].push(drago);
+		newDrago.setOwner(msg.sender);  //owner is msg.sender
 		registerDrago(drago, _name, _symbol, dragoID);
 		DragoCreated(_name, _symbol, drago, msg.sender, dragoID);
 		return true;
@@ -83,7 +85,7 @@ contract DragoFactory is Owned, DragoFactoryFace {
 	
 	function registerDrago(address _drago, string _name, string _symbol, uint _dragoID) internal {
 		DragoRegistry registry = DragoRegistry(data.dragoRegistry);
-		assert(registry.registerAs(_drago, _name, _symbol, _dragoID, msg.sender));
+		assert(registry.register(_drago, _name, _symbol, _dragoID, msg.sender));
 	}
 	
 	function setRegistry(address _newRegistry) only_owner {
@@ -113,7 +115,6 @@ contract DragoFactory is Owned, DragoFactoryFace {
 	Data data;
 	
 	string public version = 'DF0.3';
-	//uint public nextDragoID = 1;
 	address public dragoDAO = msg.sender;
 	address public owner = msg.sender;
 }
