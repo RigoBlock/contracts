76ba8f1174cb699c627fbb8b52cab5faa0211082
escursionista <gabriele@rigoinvestment.com>
2017-04-29 08:37:00 +0200

    Update and rename ErcExchangeFace.sol to ERC20ExchangeFace.sol

--- Exchange/ERC20ExchangeFace.sol
+++ Exchange/ERC20ExchangeFace.sol
@@ -4,30 +4,30 @@
 
 pragma solidity ^0.4.10;
 
-contract ErcExchangeFace {
-
+contract Exchange {
+    
 	// EVENTS
 
+	event Deposit(address token, address user, uint amount, uint balance);
+	event Withdraw(address token, address user, uint amount, uint balance);
 	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
 	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
 	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
-	event Deposit(address token, address user, uint amount, uint balance);
-	event Withdraw(address token, address user, uint amount, uint balance);
 
 	// METHODS
 
-	function deposit(address token, uint256 amount) payable {}
-	function withdraw(address token, uint256 amount) {}
+	function deposit(address token, uint256 amount) payable returns (bool success) {}
+	function withdraw(address token, uint256 amount) returns (bool success) {}
 	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
 	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
-	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
-
+	function cancelOrder(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
+	
 	function balanceOf(address _who) constant returns (uint256) {}
 	function balanceOf(address token, address user) constant returns (uint256) {}
 	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
 	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
-    	
 	function getLastOrderId() constant returns (uint) {}
-	function isActive(uint id) constant returns (bool active) {}
-	function getOwner(uint id) constant returns (address owner) {}
+	function isActive(uint id) constant returns (bool) {}
+	function getOwner(uint id) constant returns (address) {}
+	function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
 }
