6a580aed6befa1eae156d8789784e5e6a528f3da
escursionista <gabriele@rigoinvestment.com>
2017-04-29 17:06:18 +0200

    upgradeable

--- Registries/DragoRegistry.sol
+++ Registries/DragoRegistry.sol
@@ -38,7 +38,10 @@ contract DragoRegistryFace {
 	function unregister(uint _id) {}
 	function setMeta(uint _id, bytes32 _key, bytes32 _value) {}
 	function setFee(uint _fee) {}
+	function upgrade(address _newAddress) payable {}
+	function setUpgraded(uint _version) {}
 	function drain() {}
+	function kill() {}
 	
 	function dragoCount() constant returns (uint) {}
 	function drago(uint _id) constant returns (address drago, string name, string symbol, uint dragoID, address owner) {}
@@ -59,7 +62,7 @@ contract DragoRegistry is DragoRegistryFace, Owned {
 		mapping (bytes32 => bytes32) meta;
 	}
     
-	// EVENTS
+    // EVENTS
 
 	event Registered(string indexed name, string indexed symbol, uint indexed id, address drago, uint dragoID);
 	event Unregistered(string indexed name, string indexed symbol, uint indexed id);
@@ -73,7 +76,6 @@ contract DragoRegistry is DragoRegistryFace, Owned {
 	modifier when_is_symbol(string _symbol) { if (bytes(_symbol).length != 3) return; _; }
 	modifier when_has_symbol(string _symbol) { if (mapFromSymbol[_symbol] == 0) return; _; }
 	modifier only_drago_owner(uint _id) { if (dragos[_id].owner != msg.sender) return; _; }
-	//modifier only_rigoblock_drago
 	modifier when_name_free(string _name) { if (mapFromName[_name] != 0) return; _; }
 	modifier when_has_name(string _name) { if (mapFromName[_name] == 0) return; _; }
     
@@ -104,27 +106,27 @@ contract DragoRegistry is DragoRegistryFace, Owned {
 		dragos[_id].meta[_key] = _value;
 		MetaChanged(_id, _key, _value);
 	}
-
+	
 	function setFee(uint _fee) only_owner {
 		fee = _fee;
 	}
-
+	
 	function upgrade(address _newAddress) payable only_owner {
 		DragoRegistry registry = DragoRegistry(_newAddress);
 		++version;
 		registry.setUpgraded(version);
-	    	if (!address(registry).call.value(msg.value)(msg.data)) throw;	//copy old data to new externally and then upgrade
+	    if (!address(registry).call.value(msg.value)(msg.data)) throw;	//copy old data to new externally and then upgrade
 	}
 
 	function setUpgraded(uint _version) only_owner {
-    		version = _version;
+    	version = _version;
   	}
-
+	
 	function drain() only_owner {
 		if (!msg.sender.send(this.balance))
 			throw;
 	}
-
+	
 	function kill() only_owner {
 	    suicide(msg.sender);
 	}
@@ -178,7 +180,6 @@ contract DragoRegistry is DragoRegistryFace, Owned {
 	mapping (address => uint) mapFromAddress;
 	mapping (string => uint) mapFromName;
 	mapping (string => uint) mapFromSymbol;
-	address public _drago;
 	uint public fee = 0;
 	uint public version;
 	Drago[] dragos;
