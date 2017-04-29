092fa5bcbf752b2bbbf98f1cfdce6a93dccc8c7d
escursionista <gabriele@rigoinvestment.com>
2017-04-29 07:23:12 +0200

    Create AuthorityFace.sol

---
+++ Registries/Authority/AuthorityFace.sol
@@ -0,0 +1,36 @@
+//! Authority Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Auth has the possibility of blocking/unblocking single user.
+
+pragma solidity ^0.4.10;
+
+contract AuthorityFace {
+
+    // EVENTS
+  
+    event SetAuthority (address indexed authority);
+    event SetWhitelister (address indexed whitelister);
+    event WhitelistedUser(address indexed target, bool approved);
+    event WhitelistedAsset(address indexed asset, bool approved);
+    event WhitelistedExchange(address indexed exchange, bool approved);
+    event WhitelistedRegistry(address indexed registry, bool approved);
+    
+    // METHODS
+  
+    function setAuthority(address _authority) {}
+    function setWhitelister(address _whitelister) {}
+    function whitelistUser(address _target, bool _isWhitelisted) {}
+    function whitelistAsset(address _asset, bool _isWhitelisted) {}
+    function whitelistExchange(address _exchange, bool _isWhitelisted) {}
+    function whitelistRegistry(address _registry, bool _isWhitelisted) {}
+  
+    function isWhitelistedUser(address _target) constant returns (bool) {}
+    function isWhitelistedAsset(address _asset) constant returns (bool) {}
+    function isWhitelistedExchange(address _exchange) constant returns (bool) {}
+    function isWhitelistedRegistry(address _registry) constant returns (bool) {}
+    function isWhitelistedDrago(address _drago) constant returns (bool) {}
+    function getOwner() constant returns (address) {}
+    function getAuth() constant returns (address) {}
+    function getWhitelisters() constant returns (address[]) {}
+}
