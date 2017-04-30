4ae1557d00bc9b0a71d229ae66ce5eef481bc991
escursionista <gabriele@rigoinvestment.com>
2017-04-30 21:09:38 +0200

    universal exchange

--- Drago/DragoFace.sol
+++ Drago/DragoFace.sol
@@ -17,16 +17,14 @@ contract DragoFace {
 	function setTransactionFee(uint _transactionFee) {}
 	function changeFeeCollector(address _feeCollector) {}
 	function changeDragoDAO(address _dragoDAO) {}
-	function depositToExchange(address _exchange, address _token, uint256 _value) payable returns(bool success) {}
-	function depositToCFDExchange(address _cfdExchange, uint256 _value) payable returns(bool success) {}
+	function depositToExchange(address _exchange, address _token, uint256 _value) /*payable*/ returns(bool success) {}
 	function withdrawFromExchange(address _exchange, address _token, uint256 _value) returns (bool success) {}
-	function withdrawFromCFDExchange(address _cfdExchange, uint _amount) returns(bool success) {}
 	function placeOrderExchange(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce) {}
 	function placeTradeExchange(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce, address _user, uint8 _v, bytes32 _r, bytes32 _s, uint _amount) {}
-	function placeOrderCFDExchange(address _cfdExchange, address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) {}
+	function placeOrderCFDExchange(address _exchange, address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) {}
 	function cancelOrderExchange(address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
-	function cancelOrderCFDExchange(address _cfdExchange, address _cfd, uint32 _id) {}	
-	function finalizeDealCFDExchange(address _cfdExchange, address _cfd, uint24 _id) {}
+	function cancelOrderCFDExchange(address _exchange, address _cfd, uint32 _id) {}	
+	function finalizeDealCFDExchange(address _exchange, address _cfd, uint24 _id) {}
 	function setOwner(address _new) {}
 
 	function balanceOf(address _who) constant returns (uint256) {}
