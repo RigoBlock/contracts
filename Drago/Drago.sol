aa40a7b1e5960a4a2fa4dac4d5a80a51da0eeb09
escursionista <gabriele@rigoinvestment.com>
2017-04-30 19:32:10 +0200

    typo

--- Drago/Drago.sol
+++ Drago/Drago.sol
@@ -342,32 +342,3 @@ contract Drago is Owned, ERC20, SafeMath, DragoFace {
 	address public feeCollector = msg.sender;
 	mapping (address => Account) accounts;
 }
-
-contract DragoRegistry {
-
-	//EVENTS
-
-	event Registered(string indexed symbol, uint indexed id, address drago, string name);
-	event Unregistered(string indexed symbol, uint indexed id);
-	event MetaChanged(uint indexed id, bytes32 indexed key, bytes32 value);
-	
-	// METHODS
-        
-	function register(address _drago, string _name, string _symbol, uint _dragoID, address _owner) payable returns (bool) {}
-	function registerAs(address _drago, string _name, string _symbol, uint _dragoID, address _owner, address _group) payable returns (bool) {}
-	function unregister(uint _id) {}
-	function setMeta(uint _id, bytes32 _key, bytes32 _value) {}
-	function setFee(uint _fee) {}
-	function upgrade(address _newAddress) payable {}
-	function setUpgraded(uint _version) {}
-	function drain() {}
-	function kill() {}
-	
-	function dragoCount() constant returns (uint) {}
-	function drago(uint _id) constant returns (address drago, string name, string symbol, uint dragoID, address owner, address group) {}
-	function fromAddress(address _drago) constant returns (uint id, string name, string symbol, uint dragoID, address owner, address group) {}
-	function fromSymbol(string _symbol) constant returns (uint id, address drago, string name, uint dragoID, address owner, address group) {}
-	function fromName(string _name) constant returns (uint id, address drago, string symbol, uint dragoID, address owner, address group) {}
-	function meta(uint _id, bytes32 _key) constant returns (bytes32) {}
-	function getGroups(address _group) constant returns (address[]) {}
-}
