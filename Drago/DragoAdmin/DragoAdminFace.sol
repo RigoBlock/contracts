906d5b19bbfb8250d39409083e6cf3b27c76bd7a
escursionista <gabriele@rigoinvestment.com>
2017-04-30 22:03:46 +0200

    simplify interface

--- Drago/DragoAdmin/DragoAdminFace.sol
+++ Drago/DragoAdmin/DragoAdminFace.sol
@@ -6,15 +6,13 @@ pragma solidity ^0.4.10;
 
 library DragoAdminFace {
 
-	// EVENTS
+		// EVENTS
 
 	event Buy(address indexed from, address indexed to, uint256 indexed _amount, uint256 _revenue);
 	event Sell(address indexed from, address indexed to, uint256 indexed _amount, uint256 _revenue);
 	event NAV(uint sellPrice, uint buyPrice);
 	event DepositExchange(uint value, uint256 indexed _amount, address indexed who, address token , address indexed _exchange);
-	event DepositCFDExchange(uint , uint256 indexed _amount, address indexed who, address , address indexed _cfdExchange);
 	event WithdrawExchange(uint , uint256 indexed _amount, address indexed who, address , address indexed _cfdExchange);
-	event WithdrawCFDExchange(uint , uint256 indexed _amount, address indexed who, address , address indexed _cfdExchange);
 	event OrderCFD(address indexed _cfdExchange, address indexed _cfd);
 	event CancelCFD(address indexed _cfdExchange, address indexed _cfd);
 	event FinalizeCFD(address indexed _cfdExchange, address indexed _cfd);
@@ -30,9 +28,7 @@ library DragoAdminFace {
 	function changeFeeCollector(address _targetDrago, address _feeCollector) {}
 	function changeDragoDAO(address _targetDrago, address _dragoDAO) {}
 	function depositToExchange(address targetDrago, address exchange, address token, uint256 value) returns(bool) {}
-	function depositToCFDExchange(address _targetDrago, address _cfdExchange) returns(bool) {}
 	function withdrawFromExchange(address targetDrago, address exchange, address token, uint256 value) returns (bool) {}
-	function withdrawFromCFDExchange(address _targetDrago, address _cfdExchange, uint amount) returns(bool) {}
 	function placeOrderExchange(address _targetDrago, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce) {}
 	function placeTradeExchange(address _targetDrago, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce, address _user, uint8 _v, bytes32 _r, bytes32 _s, uint _amount) {}
 	function placeOrderCFDExchange(address _targetDrago, address _cfdExchange, address _cfd, bool is_stable, uint32 adjustment, uint128 stake) {}
