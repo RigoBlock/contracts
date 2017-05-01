0945d7b23fd36e5bda3db114b51b0553a5979e7e
escursionista <gabriele@rigoinvestment.com>
2017-05-01 19:20:47 +0200

    Update ExchangeFace.sol

--- Exchange/ExchangeFace.sol
+++ Exchange/ExchangeFace.sol
@@ -5,43 +5,38 @@
 pragma solidity ^0.4.10;
 
 contract ExchangeFace {
-
+    
 	// EVENTS
-	
+
+	event Deposit(address token, address user, uint amount, uint balance);
+	event Withdraw(address token, address user, uint amount, uint balance);
+	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
 	event OrderPlaced(address indexed cfd, address indexed who, bool indexed is_stable, uint32 adjustment, uint128 stake);
-	event OrderMatched(address indexed cfd, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
-	event OrderCancelled(address indexed cfd, address indexed who, uint128 stake);
+    event OrderMatched(address indexed cfd, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
+	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event OrderCancelled(address indexed cfd, uint32 indexed id, address indexed who, uint128 stake);
+	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
 	event DealFinalized(address indexed cfd, address indexed stable, address indexed leveraged, uint64 price);
-
-
-    	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
-    	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
-    	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
-    	event Deposit(address token, address user, uint amount, uint balance);
-    	event Withdraw(address token, address user, uint amount, uint balance);
 
 	// METHODS
 
-	function deposit(address token, uint256 amount) payable {}
-	function withdraw(address token, uint256 amount) {}
-	function orderCFD(bool is_stable, uint32 adjustment, uint128 stake) payable {}	//returns(uint id)
-	function order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce) {}
-	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) {}
-	function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) {}
-	function cancel(uint32 id) {}	//function cancel(uint id) returns (bool) {}
-	function finalize(uint24 id) {}
-	function moveOrder(uint id, uint quantity) returns (bool) {}
+	function deposit(address _token, uint256 _amount) payable returns (bool success) {}
+	function withdraw(address _token, uint256 _amount) returns (bool success) {}
+	function order(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce) {}
+	function orderCFD(address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) {} //returns(bool success, uint id)
+	function trade(address _tokenGet, uint _amountGet, address _tokenGive, uint amountGive, uint expires, uint nonce, address user, uint amount) {}
+	function cancelOrder(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint nonce) {}
+	function cancel(address _cfd, uint32 _id) {} // returns (bool success) {}
+	function finalize(address _cfd, uint24 _id) {}
 	
-	function balanceOf(address _who) constant returns (uint256) {}
 	function balanceOf(address token, address user) constant returns (uint256) {}
+	function balanceOf(address _who) constant returns (uint256) {}
+	function marginOf(address _who) constant returns (uint) {}
 	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
-    	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
-    	
+	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
 	function getLastOrderId() constant returns (uint) {}
-	function isActive(uint id) constant returns (bool active) {}
-	function getOwner(uint id) constant returns (address owner) {}
-	//function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
-    	//mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
-    	//mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
-    	//mapping (address => mapping (bytes32 => uint)) public orderFills;
+	function isActive(uint id) constant returns (bool) {}
+	function getOwner(uint id) constant returns (address) {}
+	function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
 }
