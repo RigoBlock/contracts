0b7b6cbeed6a0b6418efcd5bac2989ebb4bb3117
escursionista <gabriele@rigoinvestment.com>
2017-05-01 19:19:15 +0200

    universal exchange interface

---
+++ Exchange/ExchangeFace2.sol
@@ -0,0 +1,42 @@
+//! CFD Exchange Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+
+pragma solidity ^0.4.10;
+
+contract ExchangeFace {
+    
+	// EVENTS
+
+	event Deposit(address token, address user, uint amount, uint balance);
+	event Withdraw(address token, address user, uint amount, uint balance);
+	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event OrderPlaced(address indexed cfd, address indexed who, bool indexed is_stable, uint32 adjustment, uint128 stake);
+    event OrderMatched(address indexed cfd, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
+	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event OrderCancelled(address indexed cfd, uint32 indexed id, address indexed who, uint128 stake);
+	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
+	event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
+	event DealFinalized(address indexed cfd, address indexed stable, address indexed leveraged, uint64 price);
+
+	// METHODS
+
+	function deposit(address _token, uint256 _amount) payable returns (bool success) {}
+	function withdraw(address _token, uint256 _amount) returns (bool success) {}
+	function order(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce) {}
+	function orderCFD(address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) {} //returns(bool success, uint id)
+	function trade(address _tokenGet, uint _amountGet, address _tokenGive, uint amountGive, uint expires, uint nonce, address user, uint amount) {}
+	function cancelOrder(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint nonce) {}
+	function cancel(address _cfd, uint32 _id) {} // returns (bool success) {}
+	function finalize(address _cfd, uint24 _id) {}
+	
+	function balanceOf(address token, address user) constant returns (uint256) {}
+	function balanceOf(address _who) constant returns (uint256) {}
+	function marginOf(address _who) constant returns (uint) {}
+	function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
+	function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) constant returns(uint) {}
+	function getLastOrderId() constant returns (uint) {}
+	function isActive(uint id) constant returns (bool) {}
+	function getOwner(uint id) constant returns (address) {}
+	function getOrder(uint id) constant returns (uint, ERC20, uint, ERC20) {}
+}
