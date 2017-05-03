68876b53c3e2e708177607a0a1a76afaee131a94
escursionista <gabriele@rigoinvestment.com>
2017-05-03 19:48:06 +0200

    add eventful

--- Authority/AuthorityFace.sol
+++ Authority/AuthorityFace.sol
@@ -5,19 +5,20 @@
 
 pragma solidity ^0.4.10;
 
-contract AuthorityFace {
+contract Authority {
 
     // EVENTS
-  
+
     event SetAuthority (address indexed authority);
     event SetWhitelister (address indexed whitelister);
     event WhitelistedUser(address indexed target, bool approved);
     event WhitelistedAsset(address indexed asset, bool approved);
     event WhitelistedExchange(address indexed exchange, bool approved);
     event WhitelistedRegistry(address indexed registry, bool approved);
-    
+    event WhitelistedFactory(address indexed factory, bool approved);
+
     // METHODS
-  
+
     function setAuthority(address _authority, bool _isWhitelisted) {}
     function setWhitelister(address _whitelister, bool _isWhitelisted) {}
     function whitelistUser(address _target, bool _isWhitelisted) {}
@@ -25,13 +26,17 @@ contract AuthorityFace {
     function whitelistExchange(address _exchange, bool _isWhitelisted) {}
     function whitelistDrago(address _drago, bool _isWhitelisted) {}
     function whitelistRegistry(address _registry, bool _isWhitelisted) {}
-  
+    function whitelistFactory(address _factory, bool _isWhitelisted) {}
+    function setEventful(address _eventful) {}
+
     function isWhitelistedUser(address _target) constant returns (bool) {}
     function isWhitelister(address _whitelister) constant returns (bool) {}
     function isAuthority(address _authority) constant returns (bool) {}
     function isWhitelistedAsset(address _asset) constant returns (bool) {}
     function isWhitelistedExchange(address _exchange) constant returns (bool) {}
     function isWhitelistedRegistry(address _registry) constant returns (bool) {}
-    function isWhitelistedDrago(address _drago) constant returns (bool) {}
+    function isWhitelistedDrago(address _drago) constant returns (bool) {} 
+    function isWhitelistedFactory(address _factory) constant returns (bool) {}
+    function getEventful() constant returns (address) {}
     function getOwner() constant returns (address) {}
 }
