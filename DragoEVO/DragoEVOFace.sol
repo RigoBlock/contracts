3612e7732a65a2cdc1e8802055cdcfb55928086e
escursionista <gabriele@rigoinvestment.com>
2017-05-04 12:09:10 +0200

    Create DragoEVOFace.sol

---
+++ DragoEVO/DragoEVOFace.sol
@@ -0,0 +1,32 @@
+//! Authority contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Drago EVO version uses eventful to pool events.
+
+contract DragoFace {
+
+	// METHODS
+
+	function buyDrago() payable returns (bool success) {}
+	function sellDrago(uint256 _amount) returns (uint revenue, bool success) {}
+	function setPrices(uint256 _newSellPrice, uint256 _newBuyPrice) {}
+	function changeMinPeriod(uint32 _minPeriod) {}
+	function changeRatio(uint256 _ratio) {}
+	function setTransactionFee(uint _transactionFee) {}
+	function changeFeeCollector(address _feeCollector) {}
+	function changeDragoDAO(address _dragoDAO) {}
+	function depositToExchange(address _exchange, address _token, uint256 _value) returns(bool success) {}
+	function withdrawFromExchange(address _exchange, address _token, uint256 _value) returns (bool success) {}
+	function placeOrderExchange(address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce) {}
+	function placeTradeExchange(address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint _nonce, address _user, uint _amount) {}
+	function placeOrderCFDExchange(address _exchange, address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) {}
+	function cancelOrderExchange(address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires, uint nonce) {}
+	function cancelOrderCFDExchange(address _exchange, address _cfd, uint32 _id) {}
+	function finalizeDealCFDExchange(address _exchange, address _cfd, uint24 _id) {}
+	function setOwner(address _new) {}
+
+	function balanceOf(address _who) constant returns (uint256) {}
+	function getData() constant returns (string name, string symbol, uint sellPrice, uint buyPrice, uint totalSupply) {}
+	function getAdminData() constant returns (address feeCollector, address dragodAO, uint ratio, uint transactionFee, uint32 minPeriod) {}
+	function getOwner() constant returns (address) {}
+}
