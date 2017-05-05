51ede6509ce2beb24860c472b97acdc383faa7f7
escursionista <gabriele@rigoinvestment.com>
2017-05-05 16:57:04 +0200

    working library

--- DragoFactory/DragoFactoryLibrary/DragoFactoryLibrary.sol
+++ DragoFactory/DragoFactoryLibrary/DragoFactoryLibrary.sol
@@ -9,28 +9,26 @@ pragma solidity ^0.4.10;
 
 library DragoFactoryLibrary {
 
-    struct NewDrago {
-	    string name;
-	    string symbol;
-	    uint256 dragoID;
-	    address owner;
-	    address newAddress;
+	struct NewDrago {
+		string name;
+		string symbol;
+		uint256 dragoID;
+		address owner;
+		address newAddress;
 	}
 
-	event DragoCreated(string name, string symbol, address indexed drago, address indexed owner, uint dragoID);
+	modifier whitelisted_factory { Authority auth = Authority(0xDFF383e12A7939779359bf6A7f8766E123a18452); if (auth.isWhitelistedFactory(this)) _; }
 
-	function createDrago(NewDrago storage newDrago, string _name, string _symbol, address _owner, uint _dragoID, address _authority, address _eventful) returns (bool success) {
-	    Authority auth = Authority(0xDFF383e12A7939779359bf6A7f8766E123a18452);
-	    if (!auth.isWhitelistedFactory(this)) return;
-	    Drago drago = new Drago(_name, _symbol, _dragoID, _owner, _authority, _eventful);
-	    newDrago.name = _name;
-	    newDrago.symbol = _symbol;
-	    newDrago.dragoID = _dragoID;
-	    newDrago.newAddress = address(drago);
-	    newDrago.owner = _owner;
-	    DragoCreated(_name, _symbol, newDrago.newAddress, _owner, newDrago.dragoID);
-	    //Authority auth = Authority(newDrago.authority);
-	    //Authority auth = Authority(0xDFF383e12A7939779359bf6A7f8766E123a18452);
-	    //createDragoInternal(newDrago, _name, _symbol, _owner, _dragoID, _authority, _eventful);
+	event DragoCreated(string name, string symbol, address indexed drago, address indexed owner, uint dragoID, address indexed factory);
+
+	function createDrago(NewDrago storage self, string _name, string _symbol, address _owner, uint _dragoID, address _authority, address _eventful) whitelisted_factory returns (bool success) {
+		Drago drago = new Drago(_name, _symbol, _dragoID, _owner, _authority, _eventful);
+		self.name = _name;
+		self.symbol = _symbol;
+		self.dragoID = _dragoID;
+		self.newAddress = address(drago);
+		self.owner = _owner;
+		DragoCreated(_name, _symbol, address(drago), _owner, _dragoID, this);
+		return true;
 	}
 }
