e0d116ecbb237403399b00ec12208a4ccbd599c9
escursionista <gabriele@rigoinvestment.com>
2017-05-05 11:36:28 +0200

    one initial authority

--- DragoFactory/DragoFactoryLibrary/library.sol
+++ DragoFactory/DragoFactoryLibrary/library.sol
@@ -17,21 +17,23 @@ library DragoFactoryLibrary {
 	    address newAddress;
 	    address authority;
 	}
-	
-	function DragoFactoryLibrary(NewDrago storage newDrago, address _authority) {
-	    newDrago.authority = _authority;
-	}
-	
+
 	event DragoCreated(string name, string symbol, address indexed drago, address indexed owner, uint dragoID);
- 	
-	function createDrago(NewDrago storage newDrago, string _name, string _symbol, address _owner, uint _dragoID, address _authority) returns (bool) {
-	    Authority auth = Authority(_authority);
+ 
+    function DragoFactoryLibrary(NewDrago storage newDrago/*, address _authority*/) {
+        //newDrago.authority = _authority;
+        newDrago.authority = 0xf6C2c542394dA50A6e73b4ff24724Fb69Da93eb7;
+        //remember to change address for different networks
+    }
+  
+	function createDrago(NewDrago storage newDrago, string _name, string _symbol, address _owner, uint _dragoID, address _authority, address _eventful) returns (bool) {
+	    Authority auth = Authority(newDrago.authority);
 	    if (!auth.isWhitelistedFactory(msg.sender)) return;
-	    createDragoInternal(newDrago, _name, _symbol, _owner, _dragoID, _authority);
+	    createDragoInternal(newDrago, _name, _symbol, _owner, _dragoID, _authority, _eventful);
 	}
 	
-	function createDragoInternal(NewDrago storage newDrago, string _name, string _symbol, address _owner, uint _dragoID, address _authority) internal returns (bool success) {
-	    Drago drago = new Drago(_name, _symbol, _dragoID, _owner, _authority);
+	function createDragoInternal(NewDrago storage newDrago, string _name, string _symbol, address _owner, uint _dragoID, address _authority, address _eventful) internal returns (bool success) {
+	    Drago drago = new Drago(_name, _symbol, _dragoID, _owner, _authority, _eventful);
 	    drago.setOwner(_owner);
 	    newDrago.name = _name;
 	    newDrago.symbol = _symbol;
