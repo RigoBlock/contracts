1168428ac1bfdd9d116f84a6576b7de1f06d958a
escursionista <gabriele@rigoinvestment.com>
2017-05-06 11:23:16 +0200

    REMEMBER TO DEPLOY EXCHANGE WITH NEW INTERFACE!!

--- Exchange/ExchangeFace2.sol
+++ Exchange/ExchangeFace2.sol
@@ -2,7 +2,7 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.11;
 
 contract Exchange {
     
@@ -12,7 +12,7 @@ contract Exchange {
 	event Withdraw(address token, address user, uint amount, uint balance);
 	event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, address user);
 	event OrderPlaced(address indexed cfd, address indexed who, bool indexed is_stable, uint32 adjustment, uint128 stake);
-    event OrderMatched(address indexed cfd, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
+	event OrderMatched(address indexed cfd, address indexed stable, address indexed leveraged, bool is_stable, uint32 deal, uint64 strike, uint128 stake);
 	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
 	event OrderCancelled(address indexed cfd, uint32 indexed id, address indexed who, uint128 stake);
 	event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, address user);
@@ -26,9 +26,9 @@ contract Exchange {
 	function order(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) returns (uint id) {} //returns(bool success)
 	function orderCFD(address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) returns (uint32 id) {} //returns(bool success)
 	function trade(address _tokenGet, uint _amountGet, address _tokenGive, uint amountGive, uint expires, address user, uint amount) returns (bool success) {}
-	function cancelOrder(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) returns (bool success) {}
-	function cancel(address _cfd, uint32 _id) returns (bool success) {}
-	function finalize(address _cfd, uint24 _id) returns (bool success) {}
+	function cancelOrder(address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) {}
+	function cancel(address _cfd, uint32 _id) {}
+	function finalize(address _cfd, uint24 _id) {}
 
 	function balanceOf(address token, address user) constant returns (uint256) {}
 	function balanceOf(address _who) constant returns (uint256) {}
