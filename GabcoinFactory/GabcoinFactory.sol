612b856d4135cb5072c71d7f38149dea0b9dfc36
escursionista <gabriele@rigoinvestment.com>
2017-05-14 00:56:35 +0200

    gabcoin factory with library

--- GabcoinFactory/GabcoinFactory.sol
+++ GabcoinFactory/GabcoinFactory.sol
@@ -2,180 +2,189 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.11;
 
-contract GabcoinRegistry {
+ccontract DragoRegistry {
 
-	event Registered(string indexed tla, uint indexed id, address addr, string name);
-	event Unregistered(string indexed tla, uint indexed id);
+	//EVENTS
+
+	event Registered(string name, string symbol, uint id, address indexed drago, address indexed owner, address indexed group);
+	event Unregistered(string indexed symbol, uint indexed id);
 	event MetaChanged(uint indexed id, bytes32 indexed key, bytes32 value);
-
-	function register(address _drago, uint _dragoID) {}	
-	function register(address _addr, string _tla, uint _base, string _name) payable returns (bool) {}
+	
+	// METHODS
+        
+	function register(address _drago, string _name, string _symbol, uint _dragoID, address _owner) payable returns (bool) {}
+	function registerAs(address _drago, string _name, string _symbol, uint _dragoID, address _owner, address _group) payable returns (bool) {}
 	function unregister(uint _id) {}
-
-	function accountOf(uint _dragoID) constant returns (address) {}   
-	function dragoOf(address _drago) constant returns (uint) {}
-	function tokenCount() constant returns (uint) {}
-	function token(uint _id) constant returns (address addr, string tla, uint base, string name, address owner) {}
-	function fromAddress(address _addr) constant returns (uint id, string tla, uint base, string name, address owner) {}
-	function fromTLA(string _tla) constant returns (uint id, address addr, uint base, string name, address owner) {}
-	function meta(uint _id, bytes32 _key) constant returns (bytes32) {}
 	function setMeta(uint _id, bytes32 _key, bytes32 _value) {}
-}
-
-contract Gabcoin is Owned, ERC20Face {
-
-	event Buy(address indexed from, address indexed to, uint256 indexed _amount, uint256 _revenue);
-	event Sell(address indexed from, address indexed to, uint256 indexed _amount,uint256 _revenue);
-	//event DepositCasper(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _casper);
- 	//event WithdrawCasper(uint deposit, address indexed msg.sender, address _casper);
-
- 	function Gabcoin(string _dragoName,  string _dragoSymbol) {}    
-	function() payable {}		
-	function buyGabcoin() payable returns (uint amount) {}	
-	function sellGabcoin(uint256 amount) returns (uint revenue, bool success) {}
-	//function depositCasper(address _validation, address _withdrawal, address _casper) returns (bool success) {}
-	//function withdrawCasper(uint _validatorIndex, address _casper) {}
-	function changeRatio(uint256 _ratio) {}	
-	function setTransactionFee(uint _transactionFee) {}	
-	function changeFeeCollector(address _feeCollector) {}	
-	function changeCoinator(address _coinator) {}
-
-	function balanceOf(address _from) constant returns (uint256 balance) {}
-	function getVersion() constant returns (string version) {}
-	function getName() constant returns (string name) {}
-	function getSymbol() constant returns (string symbol) {}
-	function getPrice() constant returns (uint256 price) {}
-	function getTransactionFee() constant returns (uint256 transactionFee) {}
-	function getFeeCollector() constant returns (address feeCollector) {}
-	function getCasper() constant returns (address _casper) {}
+	function setFee(uint _fee) {}
+	function upgrade(address _newAddress) payable {}
+	function setUpgraded(uint _version) {}
+	function drain() {}
+	function kill() {}
+	
+	function dragoCount() constant returns (uint) {}
+	function drago(uint _id) constant returns (address drago, string name, string symbol, uint dragoID, address owner, address group) {}
+	function fromAddress(address _drago) constant returns (uint id, string name, string symbol, uint dragoID, address owner, address group) {}
+	function fromSymbol(string _symbol) constant returns (uint id, address drago, string name, uint dragoID, address owner, address group) {}
+	function fromName(string _name) constant returns (uint id, address drago, string symbol, uint dragoID, address owner, address group) {}
+	function fromNameSymbol(string _name, string _symbol) constant returns (address) {}
+	function meta(uint _id, bytes32 _key) constant returns (bytes32) {}
+	function getGroups(address _group) constant returns (address[]) {}
+	function getFee() constant returns (uint) {}
 }
 
 contract GabcoinFactoryFace is Owned {
 
 	event GabcoinCreated(string _name, address _gabcoin, address _owner, uint _gabcoinID);
-	//event GabcoinBought();
-	//event GabcoinSold();
-	//event CasperDeposit(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _casper);
- 	//event CasperWithdraw(uint deposit, address indexed msg.sender, address _casper);
 
-	function createGabcoin(string _name, string _symbol, address _owner) returns (address _gabcoin, uint _gabcoinID) {}
-	function setFee(uint _fee) {}
+	function createGabcoin(string _name, string _symbol, address _owner) returns (address gabcoin, uint gabcoinID) {}
+	function setTargetGabcoinDAO(address _targetGabcoin, address _gabcoinDAO) {}
+	function changeGabcoinDAO(address _newGabcoinDAO) {}
+	function setRegistry(address _newRegistry) {}
 	function setBeneficiary(address _gabcoinDAO) {}
+	function setFee(uint _fee) {}
 	function drain() {}
-	function() {}
-	function buyGabcoin(address targetGabcoin) payable {}
-	function sellGabcoin(address targetGabcoin, uint256 amount) {}
-	//function depositCasper(address targetGabcoin, address _casper) {}
-	//function withdrawCasper(address targetGabcoin, address _casper) {}
-	function changeRatio(address targetGabcoin, uint256 _ratio) {}
-	function setTransactionFee(address targetGabcoin, uint _transactionFee) {}
-	function changeFeeCollector(address targetGabcoin, address _feeCollector) {}
-	function changeCoinator(address targetGabcoin, address _coinator) {}
 
-	function getVersion() constant returns (string version) {}
-	function geeLastId() constant returns (uint _dragoID) {}
-	function getGabcoinDao() constant returns (address gabcoinDao) {}
-	//function getCasper() constant returns (address casper) {}
+    function getRegistry() constant returns (address) {}
+	function getStorage() constant returns (address gabcoinDAO, string version, uint nextGabcoinID) {}
+	function getNextID() constant returns (uint nextGabcoinID) {}
+	function getEventful() constant returns (address) {}
+	function getGabcoinDAO() constant returns (address gabcoinDAO) {}
+	function getVersion() constant returns (string) {}
+	function getGabcoinsByAddress(address _owner) constant returns (address[]) {}
+}
+
+library GabcoinFactoryLibrary {
+
+    struct NewGabcoin {
+	    string name;
+	    string symbol;
+	    uint256 gabcoinID;
+	    address owner;
+	    address newAddress;
+	}
+
+    modifier whitelisted_factory { Authority auth = Authority(0xDFF383e12A7939779359bf6A7f8766E123a18452); if (auth.isWhitelistedFactory(this)) _; }
+
+	function createGabcoin(NewGabcoin storage self, string _name, string _symbol, address _owner, uint _gabcoinID, address _authority) whitelisted_factory returns (bool success) {
+	    Gabcoin gabcoin = new Gabcoin(_name, _symbol, _gabcoinID, _owner, _authority);
+	    self.name = _name;
+	    self.symbol = _symbol;
+	    self.gabcoinID = _gabcoinID;
+	    self.newAddress = address(gabcoin);
+	    self.owner = _owner;
+	    return true;
+	}
 }
 
 contract GabcoinFactory is Owned, GabcoinFactoryFace {
     
-	modifier when_fee_paid { if (msg.value < fee) return; _; }
+    struct Data {
+	    uint fee;
+	    address gabcoinRegistry;
+	    address gabcoinDAO;
+	    address authority;
+	    mapping(address => address[]) gabcoins;
+	}
+	
+	event GabcoinCreated(string name, string symbol, address indexed gabcoin, address indexed owner, uint gabcoinID);
+
+	modifier when_fee_paid { if (msg.value < data.fee) return; _; }
 	modifier only_owner { if (msg.sender != owner) return; _; }
-	modifier only_gabcoin_dao { if (msg.sender != gabcoinDao) return; _; }
-	//modifier only_gabcoin_dao
-	//modifier casper_contract_only { if (_casper != casper) return; _; }
-	
-	event GabcoinCreated(string _name, address _gabcoin, address _owner, uint _gabcoinID);
-	//event GabcoinBought();
-	//event GabcoinSold();
-	//event CasperDeposit(uint msg.value, address indexed msg.sender, address indexed _validation, address indexed _withdrawal, address _casper);
- 	//event CasperWithdraw(uint deposit, address indexed msg.sender, address _casper);
-    
-	function GabcoinFactory () {}
-    
-	function createGabcoin(string _name, string _symbol, address _owner) when_fee_paid returns (address _gabcoin, uint _gabcoinID) {
-		Gabcoin newGabcoin = (new Gabcoin(_name, _symbol));
-		newGabcoins.push(address(newGabcoin));
-		created[msg.sender].push(address(newGabcoin));
-		newGabcoin.setOwner(msg.sender);  //library or new.tranfer(_from)
-		_gabcoinID = nextGabcoinID;     //decided at last to add sequential ID numbers
-		++nextGabcoinID;              //decided at last to add sequential ID numbers
-		registerGabcoin(_gabcoin, _gabcoinID);
-		GabcoinCreated(_name, address(newGabcoin), msg.sender, uint(newGabcoin));
-		return (address(newGabcoin), uint(newGabcoin));
+	modifier only_gabcoin_dao { if (msg.sender != data.gabcoinDAO) return; _; }
+    modifier whitelisted_factory { Authority auth = Authority(0xDFF383e12A7939779359bf6A7f8766E123a18452); if (auth.isWhitelistedFactory(this)) _; }
+
+	function GabcoinFactory(address _registry, address _gabcoinDAO, address _authority) {
+	    data.gabcoinRegistry = _registry;
+	    data.gabcoinDAO = _gabcoinDAO;
+	    data.authority = _authority;
+	    //owner = msg.sender; //has to be set since there was not enough space in drago to use standard Owned
+	    //REMEMBER TO SET FACTORY AS WHITELISTER WHEN CREATE A NEW ONE
+	    //SO THAT IT CAN WHITELIST MSG.SENDER AND DRAGO IMMEDIATELY
 	}
 	
-	function registerGabcoin(address _gabcoin, uint _gabcoinID) only_owner {
-		GabcoinRegistry registry = GabcoinRegistry(gabcoinRegistry);
-		registry.register(_gabcoin, _gabcoinID); //register @ registry
-		//event GabcoinRegistered
-	}
-    
-	function setFee(uint _fee) only_owner {    //exmple, uint public fee = 100 finney;
-		fee = _fee;
-	}
-    
-	function setBeneficiary(address _gabcoinDao) only_owner {
-		gabcoinDao = _gabcoinDao;
-	}
-  
-	function drain() only_owner {
-		if (!gabcoinDao.send(this.balance)) throw;
-	}
-  
-	function() {
-		throw;
-	}
-        
-	function buyGabcoin(address targetGabcoin) payable {
-		gabcoin.buyGabcoin.value(msg.value)();
-	}
-    
-	function sellGabcoin(address targetGabcoin, uint256 amount) {
-		gabcoin.sellGabcoin(amount);
-	}
-	
-	/*
-	function depositCasper(address targetGabcoin, address _casper) casper_contract_only only_owner {
-		Pos pos = Pos(_casper);
-		assert(pos.depositCasper(msg.value)(_casper));
-	}
-	
-	function withdrawCasper(address targetGabcoin, address _casper) casper_contract_only only_owner {
-		Pos pos = Pos(_casper);
-		assert(pos.withdrawCasper(msg.value)(_casper));
-	}
-	*/
-    
-	function changeRatio(address targetGabcoin, uint256 _ratio) only_gabcoin_dao {
-		gabcoin.changeRatio(_ratio);
-	}
-    
-	function setTransactionFee(address targetGabcoin, uint _transactionFee) {    //exmple, uint public fee = 100 finney;
-		gabcoin.setTransactionFee(_transactionFee);       //fee is in basis points (1 bps = 0.01%)
-	}
-    
-	function changeFeeCollector(address targetGabcoin, address _feeCollector) {
-		gabcoin.changeFeeCollector(_feeCollector);
-	}
-    
-	function changeCoinator(address targetGabcoin, address _coinator) {
-		gabcoin.changeCoinator(_coinator);
+	function createGabcoin(string _name, string _symbol) returns (bool success) {
+        DragoRegistry registry = DragoRegistry(data.gabcoinRegistry);
+        uint regFee = registry.getFee();
+        uint gabcoinID = registry.dragoCount();
+        if (!createGabcoinInternal(_name, _symbol, msg.sender, gabcoinID)) return;
+        assert(registry.register.value(regFee)(libraryData.newAddress, _name, _symbol, gabcoinID, msg.sender));
+        return true;
+    }
+    	
+    function createGabcoinInternal(string _name, string _symbol, address _owner, uint _gabcoinID) internal when_fee_paid returns (bool success) {
+	    Authority auth = Authority(data.authority);
+	    require(GabcoinFactoryLibrary.createGabcoin(libraryData, _name, _symbol, _owner, _gabcoinID, data.authority));
+		data.gabcoins[msg.sender].push(libraryData.newAddress);
+		//Eventful events = Eventful(getEventful());
+		//if (!events.createDrago(msg.sender, this, libraryData.newAddress, _name, _symbol, _gabcoinID, _owner)) return;
+		auth.whitelistGabcoin(libraryData.newAddress, true);
+		auth.whitelistUser(msg.sender, true);
+		GabcoinCreated(_name, _symbol, libraryData.newAddress, _owner, _gabcoinID); //already mapping global events in library
+		return true;
 	}
 
-	Gabcoin gabcoin = Gabcoin(targetGabcoin);
-	string public version = 'GC 0.2';
-	uint _gabcoinID = 0;
-	uint public fee = 0;
-	uint public nextGabcoinID;
-	address public gabcoinDao = msg.sender;
+    function setTargetGabcoinDAO(address _targetGabcoin, address _gabcoinDAO) only_owner {
+		Gabcoin gabcoin = Gabcoin(_targetGabcoin);
+		gabcoin.changeGabcoinDAO(_gabcoinDAO);
+	}
+	
+	function changeGabcoinDAO(address _newGabcoinDAO) only_gabcoin_dao {
+	    data.gabcoinDAO = _newGabcoinDAO;
+	}
+	
+	function setRegistry(address _newRegistry) only_owner {
+		data.gabcoinRegistry = _newRegistry;
+	}
+    
+	function setBeneficiary(address _gabcoinDAO) only_owner {
+		data.gabcoinDAO = _gabcoinDAO;
+	}
+	
+	function setFee(uint _fee) only_owner {
+		data.fee = _fee;
+	}
+
+	function drain() only_owner {
+		if (!data.gabcoinDAO.call.value(this.balance)()) throw;
+	}
+
+	function getRegistry() constant returns (address) {
+	    return (data.gabcoinRegistry);
+	}
+	
+	function getStorage() constant returns (address gabcoinDAO, string version, uint nextGabcoinID) {
+	    return (data.gabcoinDAO, version, nextGabcoinID);
+	}
+	
+	function getNextID() constant returns (uint nextGabcoinID) {
+	    DragoRegistry registry = DragoRegistry(data.gabcoinRegistry);
+	    nextGabcoinID = registry.dragoCount(); //we use the global gabcoin registry
+	}
+	
+	function getEventful() constant returns (address) {
+	    Authority auth = Authority(data.authority);
+	    return auth.getEventful();
+	}
+	
+	function getGabcoinDAO() constant returns (address) {
+	    return data.gabcoinDAO;
+	}
+	
+	function getVersion() constant returns (string) {
+	    return version;
+	}
+	
+	function getGabcoinsByAddress(address _owner) constant returns (address[]) {
+	    return data.gabcoins[msg.sender];
+	}
+	
+	GabcoinFactoryLibrary.NewGabcoin libraryData;
+
+	Data data;
+
+	string public version = 'GC 1.1.0';
 	address public owner = msg.sender;
-	address public gabcoinRegistry;
-	address[] public newGabcoins;
-	address _targetGabcoin;
-	//address public casper;
-	mapping(address => address[]) public created;
 }
