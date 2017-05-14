6ae327b4284128c666458c24468076da18437074
escursionista <gabriele@rigoinvestment.com>
2017-05-14 22:49:06 +0200

    Create GabocoinEventfulFace.sol

---
+++ EventfulGabcoin/GabocoinEventfulFace.sol
@@ -0,0 +1,6 @@
+//! Gabcoin Eventful Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Collects all events from all gabcoins
+
+pragma solidity ^0.4.11;
