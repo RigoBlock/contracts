ae39b33c78d4710592877b1d5f67699193f0baa7
escursionista <gabriele@rigoinvestment.com>
2017-05-14 22:50:35 +0200

    Create ExchangeEventfulFace.sol

---
+++ EventfulExchange/ExchangeEventfulFace.sol
@@ -0,0 +1,6 @@
+//! Exchange Eventful Interface contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Collects all events from all all assets listed on oour exchange
+
+pragma solidity ^0.4.11;
