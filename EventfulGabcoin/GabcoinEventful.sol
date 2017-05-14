c7d2a4fd1162b8fb5d4545f4e8fb4df9bfbedf09
escursionista <gabriele@rigoinvestment.com>
2017-05-14 22:48:30 +0200

    Update and rename eventfulGabcoin.sol to GabcoinEventful.sol

--- EventfulGabcoin/GabcoinEventful.sol
+++ EventfulGabcoin/GabcoinEventful.sol
@@ -1,4 +1,4 @@
-//! Eventful contract.
+//! Gabcoin Eventful contract.
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 //! Collects all events from all gabcoins
