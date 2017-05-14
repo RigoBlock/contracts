95864b8d7d27e8def365c1e6b26329e071c68bf2
escursionista <gabriele@rigoinvestment.com>
2017-05-14 22:47:06 +0200

    Create eventfulGabcoin.sol

---
+++ EventfulGabcoin/eventfulGabcoin.sol
@@ -0,0 +1,6 @@
+//! Eventful contract.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Collects all events from all gabcoins
+
+pragma solidity ^0.4.11;
