2547d5ea3ceb911fc61d3163f9c40aaf6571a474
escursionista <gabriele@rigoinvestment.com>
2017-05-15 20:01:33 +0200

    Update CFDExchange.sol

--- Exchange/CFDExchange.sol
+++ Exchange/CFDExchange.sol
@@ -162,12 +162,12 @@ contract ExchangeEventful {
 
 	function deposit(address _who, address _exchange, address _token, uint _amount) payable returns (bool success) {}
 	function withdraw(address _who, address _exchange, address _token, uint _amount) returns (bool success) {}
-	function order(address _who, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) returns (uint id) {}
-	function orderCFD(address _who, address _exchange, address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) returns (uint32 id) {}
-	function trade(address _who, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint amountGive, uint expires, address user, uint amount) {}
-	function cancelOrder(address _who, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) {}
-	function cancel(address _who, address _exchange, address _cfd, uint32 _id) {}
-	function finalize(address _who, address _exchange, address _cfd, uint24 _id) {}
+	function order(address _who, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) returns (bool success) {}
+	function orderCFD(address _who, address _exchange, address _cfd, bool _is_stable, uint32 _adjustment, uint128 _stake) returns (bool success) {}
+	function trade(address _who, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint amountGive, uint expires, address user, uint amount) returns (bool success) {}
+	function cancelOrder(address _who, address _exchange, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive, uint _expires) returns (bool success) {}
+	function cancel(address _who, address _exchange, address _cfd, uint32 _id) returns (bool success) {}
+	function finalize(address _who, address _exchange, address _cfd, uint24 _id) returns (bool success) {}
 	function addCredits(address _who, address _exchange, address _stable, uint _stable_gets, address _leveraged, uint _leveraged_gets, uint24 id) returns (bool success) {}
 }
 
