0d5b5ecf85ca41ae6c4268d025d9dcf1bfadf01a
escursionista <gabriele@rigoinvestment.com>
2017-05-16 00:42:14 +0200

    Update DragoFactoryFace.sol

--- DragoFactory/DragoFactoryFace.sol
+++ DragoFactory/DragoFactoryFace.sol
@@ -2,7 +2,7 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.11;
 
 contract DragoFactoryFace {
     
