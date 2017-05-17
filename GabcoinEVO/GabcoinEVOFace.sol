02a2e06ceb03cc15f3fac7972ca866949ea49179
escursionista <gabriele@rigoinvestment.com>
2017-05-17 18:23:46 +0200

    Update GabcoinEVOFace.sol

--- GabcoinEVO/GabcoinEVOFace.sol
+++ GabcoinEVO/GabcoinEVOFace.sol
@@ -2,6 +2,8 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
+pragma solidity ^0.4.11;
+
 contract GabcoinFace {
 
 	event Buy(address indexed from, address indexed to, uint256 indexed amount, uint256 revenue);
