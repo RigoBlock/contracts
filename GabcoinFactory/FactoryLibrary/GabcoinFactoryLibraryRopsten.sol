bd8e8c1dd31f4a0b7d38756b37ef7b8de37af653
escursionista <gabriele@rigoinvestment.com>
2017-05-30 22:55:40 +0200

    Create GabcoinFactoryLibraryRopsten.sol

---
+++ GabcoinFactory/FactoryLibrary/GabcoinFactoryLibraryRopsten.sol
@@ -0,0 +1,30 @@
+//! Gabcoin Factory library.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! ALWAYS DOUBLE CHECK ADDRESS AUTHORITY WHEN DEPLOY ON NEW NETWORK
+//! Only approved factories can access this library.
+
+pragma solidity ^0.4.11;
+
+library GabcoinFactoryLibrary {
+
+    struct NewGabcoin {
+	    string name;
+	    string symbol;
+	    uint256 gabcoinID;
+	    address owner;
+	    address newAddress;
+	}
+
+    modifier whitelisted_factory { Authority auth = Authority(0x11bBdDED5e2eeB536fb0903224ed208823A6AfE6); if (auth.isWhitelistedFactory(this)) _; }
+
+	function createGabcoin(NewGabcoin storage self, string _name, string _symbol, address _owner, uint _gabcoinID, address _authority) whitelisted_factory returns (bool success) {
+	    Gabcoin gabcoin = new Gabcoin(_name, _symbol, _gabcoinID, _owner, _authority);
+	    self.name = _name;
+	    self.symbol = _symbol;
+	    self.gabcoinID = _gabcoinID;
+	    self.newAddress = address(gabcoin);
+	    self.owner = _owner;
+	    return true;
+	}
+}
