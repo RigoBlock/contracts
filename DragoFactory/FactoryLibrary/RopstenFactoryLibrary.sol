4f6957cc53c582845d9e2bf5da544803e48f5ec3
escursionista <gabriele@rigoinvestment.com>
2017-05-30 19:25:53 +0200

    Create RopstenFactoryLibrary.sol

---
+++ DragoFactory/FactoryLibrary/RopstenFactoryLibrary.sol
@@ -0,0 +1,31 @@
+//! Drago Factory library.
+//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
+//! Released under the Apache Licence 2.
+//! Library performs computation, Factory is split and library is 20% smaller,
+//! allowing for more information stored in Factory.
+//! Only approved factories can access this library.
+
+pragma solidity ^0.4.11;
+
+library DragoFactoryLibrary {
+
+    struct NewDrago {
+	    string name;
+	    string symbol;
+	    uint256 dragoID;
+	    address owner;
+	    address newAddress;
+	}
+
+    modifier whitelisted_factory { Authority auth = Authority(0x11bBdDED5e2eeB536fb0903224ed208823A6AfE6); if (auth.isWhitelistedFactory(this)) _; }
+
+	function createDrago(NewDrago storage self, string _name, string _symbol, address _owner, uint _dragoID, address _authority) whitelisted_factory returns (bool success) {
+	    Drago drago = new Drago(_name, _symbol, _dragoID, _owner, _authority/*, _eventful*/);
+	    self.name = _name;
+	    self.symbol = _symbol;
+	    self.dragoID = _dragoID;
+	    self.newAddress = address(drago);
+	    self.owner = _owner;
+	    return true;
+	}
+}
