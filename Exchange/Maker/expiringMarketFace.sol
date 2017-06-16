39fd34c90b3ff9a98b1119145aa9f8a3fff9c868
escursionista <gabriele@rigoinvestment.com>
2017-06-16 13:27:05 +0200

    Update expiringMarketFace.sol

--- Exchange/Maker/expiringMarketFace.sol
+++ Exchange/Maker/expiringMarketFace.sol
@@ -1,1 +1,16 @@
+pragma solidity ^0.4.8;
 
+import "ds-auth/auth.sol";
+
+import "./simple_market.sol";
+
+// Simple Market with a market lifetime. When the lifetime has elapsed,
+// offers can only be cancelled (offer and buy will throw).
+
+contract ExpiringMarket is DSAuth, SimpleMarket {
+
+    function stop() auth {}
+
+    function getTime() constant returns (uint) {}
+    function isClosed() constant returns (bool closed) {}
+}
