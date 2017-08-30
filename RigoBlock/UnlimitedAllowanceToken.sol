968c1613e394f3db7eee453a6ab859df6851ec17
escursionista <gabriele@rigoinvestment.com>
2017-08-30 10:24:49 +0200

    Create UnlimitedAllowanceToken.sol

---
+++ RigoBlock/UnlimitedAllowanceToken.sol
@@ -0,0 +1,38 @@
+//! Unlimited allowance contract.
+//! By Gabriele Rigo (RigoBlock, Rigo Investment Sagl), 2017.
+//! Taken from 0x unlimited allownce contract.
+//! Released under the Apache Licence 2.
+//! Auth has the possibility of blocking/unblocking single user, drago, gabcoin, factory.
+
+pragma solidity ^0.4.16;
+
+contract UnlimitedAllowanceToken is StandardToken {
+
+    uint constant MAX_UINT = 2**256 - 1;
+    
+    /// @dev ERC20 transferFrom, modified such that an allowance of MAX_UINT represents an unlimited allowance.
+    /// @param _from Address to transfer from.
+    /// @param _to Address to transfer to.
+    /// @param _value Amount to transfer.
+    /// @return Success of transfer.
+    function transferFrom(address _from, address _to, uint _value)
+        public
+        returns (bool)
+    {
+        uint allowance = allowed[_from][msg.sender];
+        if (balances[_from] >= _value
+            && allowance >= _value
+            && balances[_to] + _value >= balances[_to]
+        ) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            if (allowance < MAX_UINT) {
+                allowed[_from][msg.sender] -= _value;
+            }
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
