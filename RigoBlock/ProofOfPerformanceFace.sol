3700ec6e2b832797956dc6d68193aaf4e3d687e4
escursionista <gabriele@rigoinvestment.com>
2017-09-02 19:12:41 +0200

    Update ProofOfPerformanceFace.sol

--- RigoBlock/ProofOfPerformanceFace.sol
+++ RigoBlock/ProofOfPerformanceFace.sol
@@ -7,4 +7,5 @@ contract ProofOfPerformanceFace {
     function calcNetworkValue() constant returns (uint256 totalAum) {}
     function calcPoolValue(uint256 _ofPool) internal constant returns (uint256 poolAum) {}
     function proofOfPerformance(uint _ofPool) constant returns (uint256 PoP) {}
+    function getPoolAddress(uint _ofPool) public constant returns (address) {}
 }
