1d27de8d20af34b7204b6aa654ab0fead8fd3fe8
escursionista <gabriele@rigoinvestment.com>
2017-09-02 18:38:51 +0200

    Update RigoTokFace.sol

--- RigoBlock/RigoTokFace.sol
+++ RigoBlock/RigoTokFace.sol
@@ -4,8 +4,8 @@
 
 pragma solidity ^0.4.10;
 
-contract RigoTokFace {
-    
+contract RigoTok {
+
     // EVENTS
 
     event TokenMinted(address indexed recipient, uint amount);
@@ -24,6 +24,7 @@ contract RigoTokFace {
     
     // CONSTANT METHODS
 
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
     function getName() constant returns (string name) {}
     function getSymbol() constant returns (string symbol) {}
     function getDecimals() constant returns (uint decimals) {}
@@ -32,4 +33,5 @@ contract RigoTokFace {
     function getMinter() constant returns (address minter) {}
     function getRigoblock() constant returns (address rigoblock) {}
     function getInflationFactor() constant returns (uint) {}
+    function totalSupply() constant returns (uint256) {}
 }
