c7c32352920543510bf7cbed22415348c0277af4
escursionista <gabriele@rigoinvestment.com>
2017-09-02 16:59:12 +0200

    Update RigoTokFace.sol

--- RigoBlock/RigoTokFace.sol
+++ RigoBlock/RigoTokFace.sol
@@ -5,8 +5,12 @@
 pragma solidity ^0.4.10;
 
 contract RigoTokFace {
+    
+    // EVENTS
 
     event TokenMinted(address indexed recipient, uint amount);
+
+    // NON-CONSTANT METHODS
 
     function RigoTok(address setMinter, address setRigoblock, uint setStartTime, uint setEndTime) {}
     function mintToken(address recipient, uint amount) external {}
@@ -14,7 +18,12 @@ contract RigoTokFace {
     function transferFrom(address sender, address recipient, uint amount) returns (bool success) {}
     function changeMintingAddress(address newAddress) {}
     function changeRigoblockAddress(address newAddress) {}
+    function setStartTime(uint _startTime) {}
+    function setEndTime(uint _endTime) {}
+    function setInflationFactor(uint _inflationFactor) {}
     
+    // CONSTANT METHODS
+
     function getName() constant returns (string name) {}
     function getSymbol() constant returns (string symbol) {}
     function getDecimals() constant returns (uint decimals) {}
@@ -22,4 +31,5 @@ contract RigoTokFace {
     function getEndTime() constant returns (uint endTime) {}
     function getMinter() constant returns (address minter) {}
     function getRigoblock() constant returns (address rigoblock) {}
+    function getInflationFactor() constant returns (uint) {}
 }
