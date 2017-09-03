acf451926124b52c879b59ede30d2f29fa47a7ca
escursionista <gabriele@rigoinvestment.com>
2017-09-03 19:11:57 +0200

    fix erc20 interface

--- Dependencies/ERC20Face.sol
+++ Dependencies/ERC20Face.sol
@@ -2,22 +2,18 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.16;
 
 contract ERC20Face {
-
-	//EVENTS
 	
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-  
-  	//METHODS
 	
   	function transfer(address _to, uint256 _value) returns (bool success) {}
 	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 	function approve(address _spender, uint256 _value) returns (bool success) {}
 
-	function totalSupply() constant returns (uint256 total) {}
-	function balanceOf(address _who) constant returns (uint256 balance) {}
+	function totalSupply() constant returns (uint256 totalSupply) {}
+	function balanceOf(address _owner) constant returns (uint256 balance) {}
 	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 }
