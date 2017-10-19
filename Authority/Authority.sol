b6ddde2f291b0c9b6a051272ab5131116e3c9b1f
escursionista <gabriele@rigoinvestment.com>
2017-10-19 17:07:12 +0200

    update lo latest plasma
    double check whether we want to map rigo token here as well

--- Authority/Authority.sol
+++ Authority/Authority.sol
@@ -3,20 +3,20 @@
 //! Released under the Apache Licence 2.
 //! Auth has the possibility of blocking/unblocking single user, drago, gabcoin, factory.
 
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 contract Owned {
-    
-	modifier only_owner { if (msg.sender != owner) return; _; }
+
+	modifier only_owner { require(msg.sender == owner); _; }
 
 	event NewOwner(address indexed old, address indexed current);
    
-	function setOwner(address _new) only_owner {
+	function setOwner(address _new) public only_owner {
 		owner = _new;
 		NewOwner(owner, _new);
 	}
-	
-	function getOwner() constant returns (address) {
+
+	function getOwner() public constant returns (address) {
 	    return owner;
 	}
 
@@ -41,35 +41,35 @@ contract AuthorityFace {
 
     // METHODS
 
-    function setAuthority(address _authority, bool _isWhitelisted) {}
-    function setWhitelister(address _whitelister, bool _isWhitelisted) {}
-    function whitelistUser(address _target, bool _isWhitelisted) {}
-    function whitelistAsset(address _asset, bool _isWhitelisted) {}
-    function whitelistExchange(address _exchange, bool _isWhitelisted) {}
-    function whitelistDrago(address _drago, bool _isWhitelisted) {}
-    function whitelistGabcoin(address _gabcoin, bool _isWhitelisted) {}
-    function whitelistRegistry(address _registry, bool _isWhitelisted) {}
-    function whitelistFactory(address _factory, bool _isWhitelisted) {}
-    function setEventful(address _eventful) {}
-    function setGabcoinEventful(address _gabcoinEventful) {}
-    function setExchangeEventful(address _exchangeEventful) {}
-    function setCasper(address _casper) {}
+    function setAuthority(address _authority, bool _isWhitelisted) public {}
+    function setWhitelister(address _whitelister, bool _isWhitelisted) public {}
+    function whitelistUser(address _target, bool _isWhitelisted) public {}
+    function whitelistAsset(address _asset, bool _isWhitelisted) public {}
+    function whitelistExchange(address _exchange, bool _isWhitelisted) public {}
+    function whitelistDrago(address _drago, bool _isWhitelisted) public {}
+    function whitelistGabcoin(address _gabcoin, bool _isWhitelisted) public {}
+    function whitelistRegistry(address _registry, bool _isWhitelisted) public {}
+    function whitelistFactory(address _factory, bool _isWhitelisted) public {}
+    function setEventful(address _eventful) public {}
+    function setGabcoinEventful(address _gabcoinEventful) public {}
+    function setExchangeEventful(address _exchangeEventful) public {}
+    function setCasper(address _casper) public {}
 
-    function isWhitelistedUser(address _target) constant returns (bool) {}
-    function isWhitelister(address _whitelister) constant returns (bool) {}
-    function isAuthority(address _authority) constant returns (bool) {}
-    function isWhitelistedAsset(address _asset) constant returns (bool) {}
-    function isWhitelistedExchange(address _exchange) constant returns (bool) {}
-    function isWhitelistedRegistry(address _registry) constant returns (bool) {}
-    function isWhitelistedDrago(address _drago) constant returns (bool) {}
-    function isWhitelistedGabcoin(address _gabcoin) constant returns (bool) {} 
-    function isWhitelistedFactory(address _factory) constant returns (bool) {}
-    function getEventful() constant returns (address) {}
-    function getGabcoinEventful() constant returns (address) {}
-    function getExchangeEventful() constant returns (address) {}
-    function getCasper() constant returns (address) {}
-    function getOwner() constant returns (address) {}
-    function getListsByGroups(string _group) constant returns (address[]) {}
+    function isWhitelistedUser(address _target) public constant returns (bool) {}
+    function isWhitelister(address _whitelister) public constant returns (bool) {}
+    function isAuthority(address _authority) public constant returns (bool) {}
+    function isWhitelistedAsset(address _asset) public constant returns (bool) {}
+    function isWhitelistedExchange(address _exchange) public constant returns (bool) {}
+    function isWhitelistedRegistry(address _registry) public constant returns (bool) {}
+    function isWhitelistedDrago(address _drago) public constant returns (bool) {}
+    function isWhitelistedGabcoin(address _gabcoin) public constant returns (bool) {} 
+    function isWhitelistedFactory(address _factory) public constant returns (bool) {}
+    function getEventful() public constant returns (address) {}
+    function getGabcoinEventful() public constant returns (address) {}
+    function getExchangeEventful() public constant returns (address) {}
+    function getCasper() public constant returns (address) {}
+    function getOwner() public constant returns (address) {}
+    function getListsByGroups(string _group) public constant returns (address[]) {}
 }
 
 contract Authority is Owned, AuthorityFace {
@@ -121,11 +121,11 @@ contract Authority is Owned, AuthorityFace {
     event NewExchangeEventful(address indexed gabcoinEventful);
     event NewCasper(address indexed casper);
 
-    modifier only_whitelister { if  (isWhitelister(msg.sender)) _; }
+    modifier only_whitelister { if (isWhitelister(msg.sender)) _; }
     modifier only_authority { if (isAuthority(msg.sender)) _; }
     modifier only_admin { if (msg.sender == owner || isWhitelister(msg.sender)) _; }
 	
-    function setAuthority(address _authority, bool _isWhitelisted) only_owner {		
+    function setAuthority(address _authority, bool _isWhitelisted) public only_owner {		
         accounts[_authority].account = _authority;
         accounts[_authority].authorized = _isWhitelisted;
         accounts[_authority].groups[_isWhitelisted].authority = _isWhitelisted;
@@ -133,15 +133,15 @@ contract Authority is Owned, AuthorityFace {
         SetAuthority(_authority);
     }
 
-    function setWhitelister(address _whitelister, bool _isWhitelisted) only_owner {
+    function setWhitelister(address _whitelister, bool _isWhitelisted) public only_owner {
         accounts[_whitelister].account = _whitelister;
         accounts[_whitelister].authorized = _isWhitelisted;
         accounts[_whitelister].groups[_isWhitelisted].whitelister = _isWhitelisted;
         types.list.push(List(_whitelister));
         SetWhitelister(_whitelister);
     }
-	
-    function whitelistUser(address _target, bool _isWhitelisted) only_whitelister {
+
+    function whitelistUser(address _target, bool _isWhitelisted) public only_whitelister {
         accounts[_target].account = _target;
         accounts[_target].authorized = _isWhitelisted;
         accounts[_target].groups[_isWhitelisted].user = _isWhitelisted;
@@ -149,7 +149,7 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedUser(_target, _isWhitelisted);
     }
 
-    function whitelistAsset(address _asset, bool _isWhitelisted) only_whitelister {
+    function whitelistAsset(address _asset, bool _isWhitelisted) public only_whitelister {
         accounts[_asset].account = _asset;
         accounts[_asset].authorized = _isWhitelisted;
         accounts[_asset].groups[_isWhitelisted].asset = _isWhitelisted;
@@ -157,7 +157,7 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedAsset(_asset, _isWhitelisted);
     }
     
-    function whitelistExchange(address _exchange, bool _isWhitelisted) only_whitelister {
+    function whitelistExchange(address _exchange, bool _isWhitelisted) public only_whitelister {
         accounts[_exchange].account = _exchange;
         accounts[_exchange].authorized = _isWhitelisted;
         accounts[_exchange].groups[_isWhitelisted].exchange = _isWhitelisted;
@@ -165,7 +165,7 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedExchange(_exchange, _isWhitelisted);
     }
     
-    function whitelistDrago(address _drago, bool _isWhitelisted) only_admin {
+    function whitelistDrago(address _drago, bool _isWhitelisted) public only_admin {
         accounts[_drago].account = _drago;
         accounts[_drago].authorized = _isWhitelisted;
         accounts[_drago].groups[_isWhitelisted].drago = _isWhitelisted;
@@ -173,7 +173,7 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedDrago(_drago, _isWhitelisted);
     }
     
-    function whitelistGabcoin(address _gabcoin, bool _isWhitelisted) only_admin {
+    function whitelistGabcoin(address _gabcoin, bool _isWhitelisted) public only_admin {
         accounts[_gabcoin].account = _gabcoin;
         accounts[_gabcoin].authorized = _isWhitelisted;
         accounts[_gabcoin].groups[_isWhitelisted].gabcoin = _isWhitelisted;
@@ -181,7 +181,7 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedDrago(_gabcoin, _isWhitelisted);
     }
     
-    function whitelistRegistry(address _registry, bool _isWhitelisted) only_admin {
+    function whitelistRegistry(address _registry, bool _isWhitelisted) public only_admin {
         accounts[_registry].account = _registry;
         accounts[_registry].authorized = _isWhitelisted;
         accounts[_registry].groups[_isWhitelisted].registry = _isWhitelisted;
@@ -189,7 +189,7 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedRegistry(_registry, _isWhitelisted);
     }
 
-    function whitelistFactory(address _factory, bool _isWhitelisted) only_admin {
+    function whitelistFactory(address _factory, bool _isWhitelisted) public only_admin {
         accounts[_factory].account = _factory;
         accounts[_factory].authorized = _isWhitelisted;
         accounts[_factory].groups[_isWhitelisted].registry = _isWhitelisted;
@@ -197,79 +197,79 @@ contract Authority is Owned, AuthorityFace {
         WhitelistedFactory(_factory, _isWhitelisted);
     }
     
-    function setEventful(address _eventful) only_owner {
+    function setEventful(address _eventful) public only_owner {
 		blocks.eventful = _eventful;
 		NewEventful(blocks.eventful);
 	}
 	
-	function setGabcoinEventful(address _gabcoinEventful) only_owner {
+	function setGabcoinEventful(address _gabcoinEventful) public only_owner {
 		blocks.gabcoinEventful = _gabcoinEventful;
 		NewGabcoinEventful(blocks.gabcoinEventful);
 	}
 	
-	function setExchangeEventful(address _exchangeEventful) only_owner {
+	function setExchangeEventful(address _exchangeEventful) public only_owner {
 		blocks.exchangeEventful = _exchangeEventful;
 		NewExchangeEventful(blocks.exchangeEventful);
 	}
 	
-	function setCasper(address _casper) only_owner {
+	function setCasper(address _casper) public only_owner {
 		blocks.casper = _casper;
 		NewCasper(blocks.casper);
 	}
 
-    function isWhitelistedUser(address _target) constant returns (bool) {
+    function isWhitelistedUser(address _target) public constant returns (bool) {
         return accounts[_target].groups[true].user;
     }
     
-    function isWhitelister(address _whitelister) constant returns (bool) {
+    function isWhitelister(address _whitelister) public constant returns (bool) {
         return accounts[_whitelister].groups[true].whitelister;
     }
     
-    function isAuthority(address _authority) constant returns (bool) {
+    function isAuthority(address _authority) public constant returns (bool) {
         return accounts[_authority].groups[true].authority;
     }
     
-    function isWhitelistedAsset(address _asset) constant returns (bool) {
+    function isWhitelistedAsset(address _asset) public constant returns (bool) {
         return accounts[_asset].groups[true].asset;
     }	
     
-    function isWhitelistedExchange(address _exchange) constant returns (bool) {
+    function isWhitelistedExchange(address _exchange) public constant returns (bool) {
         return accounts[_exchange].groups[true].exchange;
     }	
     
-    function isWhitelistedDrago(address _drago) constant returns (bool) {
+    function isWhitelistedDrago(address _drago) public constant returns (bool) {
         return accounts[_drago].groups[true].drago;
     }	
     
-    function isWhitelistedGabcoin(address _gabcoin) constant returns (bool) {
+    function isWhitelistedGabcoin(address _gabcoin) public constant returns (bool) {
         return accounts[_gabcoin].groups[true].gabcoin;
     } 
 
-    function isWhitelistedRegistry(address _registry) constant returns (bool) {
+    function isWhitelistedRegistry(address _registry) public constant returns (bool) {
         return accounts[_registry].groups[true].registry;
     }
     
-    function isWhitelistedFactory(address _factory) constant returns (bool) {
+    function isWhitelistedFactory(address _factory) public constant returns (bool) {
         return accounts[_factory].groups[true].registry;
     }
 
-    function getEventful() constant returns (address) {
+    function getEventful() public constant returns (address) {
 	    return blocks.eventful;
 	}
     
-    function getGabcoinEventful() constant returns (address) {
+    function getGabcoinEventful() public constant returns (address) {
         return blocks.gabcoinEventful;
     }
     
-    function getExchangeEventful() constant returns (address) {
+    function getExchangeEventful() public constant returns (address) {
         return blocks.exchangeEventful;
     }
     
-    function getCasper() constant returns (address) {
+    function getCasper() public constant returns (address) {
         return blocks.casper;
     }
     
-    function getListsByGroups(string _group) constant returns (address[]) {
+    function getListsByGroups(string _group) public constant returns (address[]) {
         return types.mapFromGroup[_group];
     }
 
