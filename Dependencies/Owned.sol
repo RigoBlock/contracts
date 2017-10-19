811c62085544a8a3c07751d5af169c31e9a668d5
escursionista <gabriele@rigoinvestment.com>
2017-10-19 17:11:18 +0200

    Update Owned.sol

--- Dependencies/Owned.sol
+++ Dependencies/Owned.sol
@@ -1,19 +1,21 @@
-//! Hedge Fund contract.
+//! Owned contract.
 //! By Gabriele Rigo (Rigo Investment), 2017.
 //! Released under the Apache Licence 2.
 
+pragma solidity ^0.4.18;
+
 contract Owned {
-    
-	modifier only_owner { if (msg.sender != owner) return; _; }
+
+	modifier only_owner { require(msg.sender == owner); _; }
 
 	event NewOwner(address indexed old, address indexed current);
    
-	function setOwner(address _new) only_owner {
+	function setOwner(address _new) public only_owner {
 		owner = _new;
 		NewOwner(owner, _new);
 	}
-	
-	function getOwner() constant returns (address) {
+
+	function getOwner() public constant returns (address) {
 	    return owner;
 	}
 
