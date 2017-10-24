dd1d743bd199368858c869baf0685a932f70f944
escursionista <gabriele@rigoinvestment.com>
2017-10-24 23:53:04 +0200

    Update NAV.sol

--- NAV/NAV.sol
+++ NAV/NAV.sol
@@ -2,14 +2,20 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.18;
+
+contract NAVFace {
+
+  function estimateNAV() public returns (uint) {}
+}
 
 contract NAV is NAVFace {
 
-  function estimateNAV() returns (uint) {
+  function estimateNAV() public returns (uint) {
     return estimateAUM();
   }
 
+  //numAssignedAssets has to be defined
   function estimateAUM(address _drago) internal returns (uint256 aum) {
     for (uint i = 0; i < numAssignedAssets; ++i) {
       address _asset = address(asset(i));
