e56e3d6a732e8e5601f87c9ba9f544ca8e4234cc
escursionista <gabriele@rigoinvestment.com>
2017-10-24 23:53:24 +0200

    Update NAVFace.sol

--- NAV/NAVFace.sol
+++ NAV/NAVFace.sol
@@ -2,9 +2,9 @@
 //! By Gabriele Rigo (Rigo Investment Sagl), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.18;
 
 contract NAVFace {
 
-  function estimateNAV() returns (uint) {}
+  function estimateNAV() public returns (uint) {}
 }
