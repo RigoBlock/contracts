5a19cc85f985fab592b6fbc6e248ed389ed9bfa6
escursionista <gabriele@rigoinvestment.com>
2017-12-02 21:49:18 +0100

    Update DragoStub.sol

--- DragoEVO/DragoStub.sol
+++ DragoEVO/DragoStub.sol
@@ -18,3 +18,129 @@
 //! version 2.0 (see above) or the MIT-license, at your choice.
 
 pragma solidity ^0.4.19;
+
+contract Authority {
+
+    // METHODS
+
+    function setAuthority(address _authority, bool _isWhitelisted) public {}
+    function setWhitelister(address _whitelister, bool _isWhitelisted) public {}
+    function whitelistUser(address _target, bool _isWhitelisted) public {}
+    function whitelistAsset(address _asset, bool _isWhitelisted) public {}
+    function whitelistExchange(address _exchange, bool _isWhitelisted) public {}
+    function whitelistDrago(address _drago, bool _isWhitelisted) public {}
+    function whitelistGabcoin(address _gabcoin, bool _isWhitelisted) public {}
+    function whitelistRegistry(address _registry, bool _isWhitelisted) public {}
+    function whitelistFactory(address _factory, bool _isWhitelisted) public {}
+    function setEventful(address _eventful) public {}
+    function setGabcoinEventful(address _gabcoinEventful) public {}
+    function setExchangeEventful(address _exchangeEventful) public {}
+    function setCasper(address _casper) public {}
+    //TODO: implement the below
+    function setAdapter(address _exchange) public {}
+
+    function isWhitelistedUser(address _target) public constant returns (bool) {}
+    function isWhitelister(address _whitelister) public constant returns (bool) {}
+    function isAuthority(address _authority) public constant returns (bool) {}
+    function isWhitelistedAsset(address _asset) public constant returns (bool) {}
+    function isWhitelistedExchange(address _exchange) public constant returns (bool) {}
+    function isWhitelistedRegistry(address _registry) public constant returns (bool) {}
+    function isWhitelistedDrago(address _drago) public constant returns (bool) {}
+    function isWhitelistedGabcoin(address _gabcoin) public constant returns (bool) {} 
+    function isWhitelistedFactory(address _factory) public constant returns (bool) {}
+    function getEventful() public constant returns (address) {}
+    function getGabcoinEventful() public constant returns (address) {}
+    function getExchangeEventful() public constant returns (address) {}
+    function getCasper() public constant returns (address) {}
+    function getOwner() public constant returns (address) {}
+    //TODO: below function does not return correct group
+    function getListsByGroups(string _group) public constant returns (address[]) {}
+    //TODO: implement below function
+    function getAdapter(address _exchange) public constant returns (address) {}
+    function getDragoLibrary() public constant returns (address) {}
+    
+}
+
+contract Drago {
+    function Drago(string _dragoName,  string _dragoSymbol, uint _dragoID, address _owner, address _authority) internal {
+        AUTHORITY = _authority;
+        // Signature of the Drago Library's init function
+        // bytes4 sig = bytes4(sha3("Drago(string,string,uint,address,address)"));
+        bytes4 sig = 0x86cc4621;
+
+        // Compute the size of the call data : arrays has 2
+        // 32bytes for offset and length, plus 32bytes per element ;
+        // plus 2 32bytes for each uint
+        uint argarraysize = (5);
+        uint argsize = (2 + argarraysize) * 32;
+
+        // total data size:
+        //   4 bytes for the signature + the arguments size
+        uint size = 4 + argsize;
+        bytes32 m_data = _malloc(size);
+
+        // copy the signature and arguments data to the
+        // data pointer
+        assembly {
+            // Add the signature first to memory
+            mstore(m_data, sig)
+            // Add the call data, which is at the end of the code
+            codecopy(add(m_data, 0x4), sub(codesize, argsize), argsize)
+        }
+
+        // execute the call (to initWallet)
+        _call(m_data, size);
+    }
+
+    // delegate any contract calls to
+    // the library
+    function() payable {
+        uint size = msg.data.length;
+        bytes32 m_data = _malloc(size);
+
+        assembly {
+            calldatacopy(m_data, 0x0, size)
+        }
+
+        bytes32 m_result = _call(m_data, size);
+
+        assembly {
+            return(m_result, 0x20)
+        }
+    }
+
+    // allocate the given size in memory and return
+    // the pointer
+    function _malloc(uint size) private returns(bytes32) {
+        bytes32 m_data;
+
+        assembly {
+            // Get free memory pointer and update it
+            m_data := mload(0x40)
+            mstore(0x40, add(m_data, size))
+        }
+
+        return m_data;
+    }
+
+    // make a delegatecall to the target contract, given the
+    // data located at m_data, that has the given size
+    //
+    // @returns A pointer to memory which contain the 32 first bytes
+    //          of the delegatecall output
+    function _call(bytes32 m_data, uint size) private returns(bytes32) {
+        Authority auth = Authority(AUTHORITY);
+        address target = auth.getDragoLibrary();
+        bytes32 m_result = _malloc(32);
+        bool failed;
+
+        assembly {
+            failed := iszero(delegatecall(sub(gas, 10000), target, m_data, size, m_result, 0x20))
+        }
+
+        require(!failed);
+        return m_result;
+    }
+    
+    address public AUTHORITY;
+}
