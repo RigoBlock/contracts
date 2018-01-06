8fe31877470dd691856f93e3b9db808479161a1f
escursionista <gabriele@rigoinvestment.com>
2018-01-06 18:28:09 +0100

    token for testing

--- RigoBlock/RigoTok.sol
+++ RigoBlock/RigoTok.sol
@@ -2,47 +2,47 @@
 //! By Gabriele Rigo (Rigo Investment), 2017.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.19;
 
 contract SafeMath {
 
-    function safeMul(uint a, uint b) internal returns (uint) {
+    function safeMul(uint a, uint b) internal pure returns (uint) {
         uint c = a * b;
         assert(a == 0 || c / a == b);
         return c;
     }
     
-    function safeDiv(uint a, uint b) internal returns (uint) {
+    function safeDiv(uint a, uint b) internal pure returns (uint) {
         assert(b > 0);
         uint c = a / b;
         assert(a == b * c + a % b);
         return c;
     }
 
-    function safeSub(uint a, uint b) internal returns (uint) {
+    function safeSub(uint a, uint b) internal pure returns (uint) {
         assert(b <= a);
         return a - b;
     }
 
-    function safeAdd(uint a, uint b) internal returns (uint) {
+    function safeAdd(uint a, uint b) internal pure returns (uint) {
         uint c = a + b;
         assert(c>=a && c>=b);
         return c;
     }
     
-    function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    function max64(uint64 a, uint64 b) internal pure returns (uint64) {
         return a >= b ? a : b;
     }
 
-    function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    function min64(uint64 a, uint64 b) internal pure returns (uint64) {
         return a < b ? a : b;
     }
 
-    function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    function max256(uint256 a, uint256 b) internal pure returns (uint256) {
         return a >= b ? a : b;
     }
 
-    function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    function min256(uint256 a, uint256 b) internal pure returns (uint256) {
         return a < b ? a : b;
     }
 }
@@ -52,18 +52,18 @@ contract ERC20Face {
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 	
-  	function transfer(address _to, uint256 _value) returns (bool success) {}
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-	function approve(address _spender, uint256 _value) returns (bool success) {}
+  	function transfer(address _to, uint256 _value) public returns (bool success) {}
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+	function approve(address _spender, uint256 _value) public returns (bool success) {}
 
-	function totalSupply() constant returns (uint256 totalSupply) {}
-	function balanceOf(address _owner) constant returns (uint256 balance) {}
-	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+	function totalSupply() public constant returns (uint256 totalSupply) {}
+	function balanceOf(address _owner) public constant returns (uint256 balance) {}
+	function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {}
 }
 
 contract ERC20 is ERC20Face {
 
-	function transfer(address _to, uint256 _value) returns (bool success) {
+	function transfer(address _to, uint256 _value) public returns (bool success) {
 		if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
 			balances[msg.sender] -= _value;
 			balances[_to] += _value;
@@ -72,7 +72,7 @@ contract ERC20 is ERC20Face {
 		} else { return false; }
 	}
 
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
 		if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
 			balances[_to] += _value;
 			balances[_from] -= _value;
@@ -82,17 +82,17 @@ contract ERC20 is ERC20Face {
 		} else { return false; }
 	}
 
-	function balanceOf(address _owner) constant returns (uint256 balance) {
+	function balanceOf(address _owner) public constant returns (uint256 balance) {
 		return balances[_owner];
 	}
 
-	function approve(address _spender, uint256 _value) returns (bool success) {
+	function approve(address _spender, uint256 _value) public returns (bool success) {
 		allowed[msg.sender][_spender] = _value;
 		Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
-	function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+	function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
 		return allowed[_owner][_spender];
 	}
 
@@ -140,28 +140,28 @@ contract RigoTokFace {
 
     // NON-CONSTANT METHODS
 
-    function RigoTok(address setMinter, address setRigoblock, uint setStartTime, uint setEndTime) {}
+    function RigoTok(address setMinter, address setRigoblock, uint setStartTime, uint setEndTime) public {}
     function mintToken(address recipient, uint amount) external {}
-    function transfer(address recipient, uint amount) returns (bool success) {}
-    function transferFrom(address sender, address recipient, uint amount) returns (bool success) {}
-    function changeMintingAddress(address newAddress) {}
-    function changeRigoblockAddress(address newAddress) {}
-    function setStartTime(uint _startTime) {}
-    function setEndTime(uint _endTime) {}
-    function setInflationFactor(uint _inflationFactor) {}
+    function transfer(address recipient, uint amount) public returns (bool success) {}
+    function transferFrom(address sender, address recipient, uint amount) public returns (bool success) {}
+    function changeMintingAddress(address newAddress) public {}
+    function changeRigoblockAddress(address newAddress) public {}
+    function setStartTime(uint _startTime) public {}
+    function setEndTime(uint _endTime) public {}
+    function setInflationFactor(uint _inflationFactor) public {}
     
     // CONSTANT METHODS
 
     //function balanceOf(address _owner) constant returns (uint256 balance) {}
     //function totalSupply() constant returns (uint256 totalSupply) {}
-    function getName() constant returns (string) {}
-    function getSymbol() constant returns (string) {}
-    function getDecimals() constant returns (uint) {}
-    function getStartTime() constant returns (uint) {}
-    function getEndTime() constant returns (uint) {}
-    function getMinter() constant returns (address) {}
-    function getRigoblock() constant returns (address) {}
-    function getInflationFactor() constant returns (uint) {}
+    function getName() public constant returns (string) {}
+    function getSymbol() public constant returns (string) {}
+    function getDecimals() public constant returns (uint) {}
+    function getStartTime() public constant returns (uint) {}
+    function getEndTime() public constant returns (uint) {}
+    function getMinter() public constant returns (address) {}
+    function getRigoblock() public constant returns (address) {}
+    function getInflationFactor() public constant returns (uint) {}
 }
 
 contract RigoTok is UnlimitedAllowanceToken, SafeMath, RigoTokFace { //UnlimitedAllowanceToken is ERC20
@@ -189,7 +189,9 @@ contract RigoTok is UnlimitedAllowanceToken, SafeMath, RigoTokFace { //Unlimited
     
     // MOTHODS
 
-    function RigoTok(address setMinter, address setRigoblock, uint setStartTime, uint setEndTime) {
+    function RigoTok(address setMinter, address setRigoblock, uint setStartTime, uint setEndTime) 
+        public
+    {
         minter = setMinter;
         rigoblock = setRigoblock;
         startTime = setStartTime;
@@ -202,68 +204,67 @@ contract RigoTok is UnlimitedAllowanceToken, SafeMath, RigoTokFace { //Unlimited
         totalSupply = safeAdd(totalSupply, amount);
     }
 
-    function transfer(address recipient, uint amount) is_later_than(endTime) returns (bool success) {
+    function transfer(address recipient, uint amount) is_later_than(endTime) public returns (bool success) {
         return super.transfer(recipient, amount);
     }
 
-    function transferFrom(address sender, address recipient, uint amount) is_later_than(endTime) returns (bool success) {
+    function transferFrom(address sender, address recipient, uint amount) public is_later_than(endTime) returns (bool success) {
         return super.transferFrom(sender, recipient, amount);
     }
 
-    function changeMintingAddress(address newAddress) only_rigoblock { 
+    function changeMintingAddress(address newAddress) public only_rigoblock { 
         minter = newAddress; 
     }
 
-    function changeRigoblockAddress(address newAddress) only_rigoblock {
+    function changeRigoblockAddress(address newAddress) public only_rigoblock {
         rigoblock = newAddress;
     }
     
-    function setStartTime(uint _startTime) only_rigoblock {
+    function setStartTime(uint _startTime) public only_rigoblock {
         startTime = _startTime;
     }
     
-    function setEndTime(uint _endTime) only_rigoblock {
+    function setEndTime(uint _endTime) public only_rigoblock {
         endTime = _endTime;
     }
    
-    function setInflationFactor(uint _inflationFactor) only_rigoblock {
+    function setInflationFactor(uint _inflationFactor) public only_rigoblock {
         inflationFactor = _inflationFactor;
     }
 
     // CONSTANT METHODS
 
-    function getName() constant returns (string) {
+    function getName() public constant returns (string) {
         return name;
     }
     
-    function getSymbol() constant returns (string) {
+    function getSymbol() public constant returns (string) {
         return symbol;
     }
     
-    function getDecimals() constant returns (uint) {
+    function getDecimals() public constant returns (uint) {
         return decimals;
     }
     
-    function getStartTime() constant returns (uint) {
+    function getStartTime() public constant returns (uint) {
         return startTime;
     }
     
-    function getEndTime() constant returns (uint) {
+    function getEndTime() public constant returns (uint) {
         return endTime;
     }
     
-    function getMinter() constant returns (address) {
+    function getMinter() public constant returns (address) {
         return minter;
     }
     
-    function getRigoblock() constant returns (address) {
+    function getRigoblock() public constant returns (address) {
         return rigoblock;
     }
     
-    function getInflationFactor() constant returns (uint) {
+    function getInflationFactor() public constant returns (uint) {
         return inflationFactor;
     }
-    
    
     string public constant name = "Rigo Token";
     string public constant symbol = "GRG";
