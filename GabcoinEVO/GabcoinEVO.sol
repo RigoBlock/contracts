62fa60a0bf3793e1f633cd075278fd809d6b24b4
escursionista <gabriele@rigoinvestment.com>
2018-01-18 23:24:19 +0100

    add buyonbehalf to gabcoinface

--- GabcoinEVO/GabcoinEVO.sol
+++ GabcoinEVO/GabcoinEVO.sol
@@ -177,9 +177,10 @@ contract GabcoinFace {
 	function deposit(address _token, uint _amount) payable returns (bool success) {}
 	function withdraw(address _token, uint _amount) returns (bool success) {}
 	function buyGabcoin() payable returns (bool success) {}
+	function buyGabcoinOnBehalf(address _hodler) payable returns (bool success) {}
 	function sellGabcoin(uint256 amount) returns (bool success) {}
 	function depositCasper(address _validation, address _withdrawal, uint _amount) returns (bool success) {}
-    function withdrawCasper(uint _validatorIndex) {}
+	function withdrawCasper(uint _validatorIndex) {}
 	function changeRatio(uint256 _ratio) {}	
 	function setTransactionFee(uint _transactionFee) {}	
 	function changeFeeCollector(address _feeCollector) {}	
@@ -187,7 +188,7 @@ contract GabcoinFace {
 	function updatePrice() {}
 	function changeMinPeriod(uint32 _minPeriod) {}
 
-    function getMinPeriod() constant returns (uint32) {}
+	function getMinPeriod() constant returns (uint32) {}
 	function balanceOf(address _from) constant returns (uint) {}
 	function getVersion() constant returns (string) {}
 	function getName() constant returns (string) {}
