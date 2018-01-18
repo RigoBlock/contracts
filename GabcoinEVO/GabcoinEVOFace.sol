0cd78313d6c44de1f4f231bc9713e4388a212bd8
escursionista <gabriele@rigoinvestment.com>
2018-01-18 23:27:39 +0100

    add buygabcoinonebehalf

--- GabcoinEVO/GabcoinEVOFace.sol
+++ GabcoinEVO/GabcoinEVOFace.sol
@@ -1,8 +1,23 @@
-//! Gabcoin EVO contract.
-//! By Gabriele Rigo (Rigo Investment Sagl), 2017.
-//! Released under the Apache Licence 2.
+//! the Gabcoin EVO contract.
+//!
+//! Copyright 2017-2018 Gabriele Rigo, RigoBlock, Rigo Investment Sagl.
+//!
+//! Licensed under the Apache License, Version 2.0 (the "License");
+//! you may not use this file except in compliance with the License.
+//! You may obtain a copy of the License at
+//!
+//!     http://www.apache.org/licenses/LICENSE-2.0
+//!
+//! Unless required by applicable law or agreed to in writing, software
+//! distributed under the License is distributed on an "AS IS" BASIS,
+//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//! See the License for the specific language governing permissions and
+//! limitations under the License.
+//!
+//! This code may be distributed under the terms of the Apache Licence
+//! version 2.0 (see above) or the MIT-license, at your choice
 
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.19;
 
 contract GabcoinFace {
 
@@ -15,9 +30,10 @@ contract GabcoinFace {
 	function deposit(address _token, uint _amount) payable returns (bool success) {}
 	function withdraw(address _token, uint _amount) returns (bool success) {}
 	function buyGabcoin() payable returns (bool success) {}
+	function buyGabcoinOnBehalf(address _hodler) payable returns (bool success) {}
 	function sellGabcoin(uint256 amount) returns (bool success) {}
 	function depositCasper(address _validation, address _withdrawal, uint _amount) returns (bool success) {}
-    	function withdrawCasper(uint _validatorIndex) {}
+	function withdrawCasper(uint _validatorIndex) {}
 	function changeRatio(uint256 _ratio) {}	
 	function setTransactionFee(uint _transactionFee) {}	
 	function changeFeeCollector(address _feeCollector) {}	
@@ -25,7 +41,7 @@ contract GabcoinFace {
 	function updatePrice() {}
 	function changeMinPeriod(uint32 _minPeriod) {}
 
-    	function getMinPeriod() constant returns (uint32) {}
+	function getMinPeriod() constant returns (uint32) {}
 	function balanceOf(address _from) constant returns (uint) {}
 	function getVersion() constant returns (string) {}
 	function getName() constant returns (string) {}
