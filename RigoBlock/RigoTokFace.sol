68f3f300d181a7f00343069b7a763325e175713c
escursionista <gabriele@rigoinvestment.com>
2018-01-20 18:58:54 +0100

    Update RigoTokFace.sol

--- RigoBlock/RigoTokFace.sol
+++ RigoBlock/RigoTokFace.sol
@@ -1,8 +1,20 @@
-//! RigoBlock Token Interface contract.
-//! By Gabriele Rigo (Rigo Investment), 2017.
-//! Released under the Apache Licence 2.
-
-pragma solidity ^0.4.10;
+//! the RigoBlock Token Interface contract.
+//!
+//! The proof-of-performance contract.
+//!
+//! Copyright 2017-2018 Gabriele Rigo, RigoBlock, Rigo Investment Sagl.
+//!
+//! Licensed under the Apache License, Version 2.0 (the "License");
+//! you may not use this file except in compliance with the License.
+//! You may obtain a copy of the License at
+//!
+//!     http://www.apache.org/licenses/LICENSE-2.0
+//!
+//! Unless required by applicable law or agreed to in writing, software
+//! distributed under the License is distributed on an "AS IS" BASIS,
+//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//! See the License for the specific language governing permissions and
+//! limitations under the License.
 
 contract RigoTokFace {
 
@@ -12,26 +24,21 @@ contract RigoTokFace {
 
     // NON-CONSTANT METHODS
 
-    function RigoTok(address setMinter, address setRigoblock, uint setStartTime, uint setEndTime) {}
+    function RigoTok(address setMinter, address setRigoblock) public {}
     function mintToken(address recipient, uint amount) external {}
-    function transfer(address recipient, uint amount) returns (bool success) {}
-    function transferFrom(address sender, address recipient, uint amount) returns (bool success) {}
-    function changeMintingAddress(address newAddress) {}
-    function changeRigoblockAddress(address newAddress) {}
-    function setStartTime(uint _startTime) {}
-    function setEndTime(uint _endTime) {}
-    function setInflationFactor(uint _inflationFactor) {}
-    
+    //function transfer(address recipient, uint amount) public returns (bool success) {}
+    //function transferFrom(address sender, address recipient, uint amount) public returns (bool success) {}
+    function changeMintingAddress(address newAddress) public {}
+    function changeRigoblockAddress(address newAddress) public {}
+
     // CONSTANT METHODS
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
-    function totalSupply() constant returns (uint256 totalSupply) {}
-    function getName() constant returns (string) {}
-    function getSymbol() constant returns (string) {}
-    function getDecimals() constant returns (uint) {}
-    function getStartTime() constant returns (uint) {}
-    function getEndTime() constant returns (uint) {}
-    function getMinter() constant returns (address) {}
-    function getRigoblock() constant returns (address) {}
-    function getInflationFactor() constant returns (uint) {}
+    //function balanceOf(address _owner) constant returns (uint256 balance) {}
+    //function totalSupply() constant returns (uint256 totalSupply) {}
+    function getName() public constant returns (string) {}
+    function getSymbol() public constant returns (string) {}
+    function getDecimals() public constant returns (uint) {}
+    function getMinter() public constant returns (address) {}
+    function getRigoblock() public constant returns (address) {}
+    function getInflationFactor(address _group) public constant returns (uint) {}
 }
